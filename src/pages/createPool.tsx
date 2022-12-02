d3b4d21059280ad4a389e056b96fddbbf421ecc4
Muhammad Usman Shamas <muhammadusman5649@gmail.com>
2022-12-02 13:16:14 +0500

    New changes

--- src/pages/createPool.tsx
+++ src/pages/createPool.tsx
@@ -1,25 +1,23 @@
 // @ts-nocheck
-import { useState, useEffect } from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { ethers } from 'ethers';
-import swal from 'sweetalert';
-import { getUserSession } from '../store/localStorage';
-import "./Application.css"
-import BottomBar from '../components/bottomBar/BottomBar';
-import Button from '../components/button/Button';
-import { H2, H3 } from '../components/text/Text';
-import { Col, Container, Row } from '../components/blocks/Blocks';
-import { Form } from '../components/form/Form';
-import { StyledDropBtn } from '../components/form/dropdownInput/DropdownInput.styles';
-import TokenModal from '../components/OwnTokenModal';
-import TokenModal1 from '../components/CustomToken';
-import Pools from '../components/pools';
+import { useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { ethers } from "ethers";
+import swal from "sweetalert";
+import { getUserSession } from "../store/localStorage";
+import "./Application.css";
+import BottomBar from "../components/bottomBar/BottomBar";
+import Button from "../components/button/Button";
+import { H2, H3 } from "../components/text/Text";
+import { Col, Container, Row } from "../components/blocks/Blocks";
+import { Form } from "../components/form/Form";
+import { StyledDropBtn } from "../components/form/dropdownInput/DropdownInput.styles";
+import TokenModal from "../components/OwnTokenModal";
+import TokenModal1 from "../components/CustomToken";
+import Pools from "../components/pools";
 
-import abi from '../config/sushiswap.json';
-import ercabi from '../config/erc20.json';
-import factory from '../config/pool.json';
-
-
+import abi from "../config/sushiswap.json";
+import ercabi from "../config/erc20.json";
+import factory from "../config/pool.json";
 
 export default function CreatePool({ lightMode }: any) {
   const { ethereum } = window;
@@ -28,49 +26,42 @@ export default function CreatePool({ lightMode }: any) {
   const [tokenTwo, setTokenTwo] = useState<any | null>(null);
   const [walletAddress, setWalletAddress] = useState(null);
 
-  const [tokenOneAmount, setTokenOneAmount] = useState('');
-  const [tokenTwoAmount, setTokenTwoAmount] = useState('');
+  const [tokenOneAmount, setTokenOneAmount] = useState("");
+  const [tokenTwoAmount, setTokenTwoAmount] = useState("");
 
   const [dtoken, setDTokens] = useState<any[]>([]);
   const [wallet, setWallet] = useState();
   const [values, setValues] = useState([]);
 
-  
   useEffect(() => {
     let walletData: any;
     let sessionData = getUserSession();
     if (sessionData) {
       walletData = JSON.parse(sessionData);
       setWallet(walletData.address);
-     
     }
   });
 
   const StableTOKEN = [
     {
-      id: '0xDB6bbEBdF9515f308e9d9690aeF0796d4fF7F999',
-      name: 'USDC',
-      symbol: 'USDC',
+      id: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+      name: "USDC",
+      symbol: "USDC",
     },
     {
-      id: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
-      name: 'USDT',
-      symbol: 'USDT',
+      id: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+      name: "USDT",
+      symbol: "USDT",
     },
     {
-      id: '0xDF1742fE5b0bFc12331D8EAec6b478DfDbD31464',
-      name: 'DAI',
-      symbol: 'DAI',
+      id: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
+      name: "DAI",
+      symbol: "DAI",
     },
     {
-      id: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-      name: 'WETH',
-      symbol: 'WETH',
-    },
-    {
-      id: '0x9D575a9bF57a5e24a99D29724B86ca021A2b0435',
-      name: 'ETH',
-      symbol: 'ETH',
+      id: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+      name: "WETH",
+      symbol: "WETH",
     },
   ];
 
@@ -103,8 +94,8 @@ export default function CreatePool({ lightMode }: any) {
       tokenOne.contract_address,
       tokenTwo.id
     );
-    const contractAddress = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
-    const factoryAddress = '0x87e49e9B403C91749dCF89be4ab1d400CBD4068C';
+    const contractAddress = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+    const factoryAddress = "0x87e49e9B403C91749dCF89be4ab1d400CBD4068C";
     const contractABI = factory;
     const tokenABI = ercabi.abi;
     // const factoryABI = factory;
@@ -114,7 +105,7 @@ export default function CreatePool({ lightMode }: any) {
     const amount1: any = tokenOneAmount;
     const amount2: any = tokenTwoAmount;
 
-    if (amount1 === amount2) {
+    try {
       const amount1min: any = 0;
       const amount2min: any = 0;
 
@@ -140,7 +131,7 @@ export default function CreatePool({ lightMode }: any) {
 
       await TOKEN.approve(factoryAddress, amountIn1);
       await TOKENB.approve(factoryAddress, amountIn2);
-console.log(amountIn1,amountIn2,amount1Min,amount2Min)
+      console.log(amountIn1, amountIn2, amount1Min, amount2Min);
       const hx = await factory.createLp(
         tokenAddress,
         tokenAddressb,
@@ -149,61 +140,62 @@ console.log(amountIn1,amountIn2,amount1Min,amount2Min)
         amount1Min,
         amount2Min,
         {
-          gasLimit: 500000,
+          gasLimit: provider.getGasPrice(),
         }
       );
 
-      const hash =await hx.wait();
-      console.log(hash,hx);
-if(hash){
-  swal({
-    title: "Pool Deployed",
-    text: "Your Pool is Deployed.",
-    icon: "success",
+      const hash = await hx.wait();
+      console.log(hash, hx);
+      if (hash) {
+        swal({
+          title: "Pool Deployed",
+          text: "Your Pool is Deployed.",
+          icon: "success",
 
-    buttons: {
-      ok: "CLOSE!",
-      Transaction: {
-        value: "Transaction",
-      },
-
-    },
-  })
-    .then((value) => {
-      switch (value) {
-        case "Transaction":
-
-          window.open(
-            `https://arbiscan.io/tx${hx.hash}`, "_blank");
-          break;
-        default:
-
+          buttons: {
+            ok: "CLOSE!",
+            Transaction: {
+              value: "Transaction",
+            },
+          },
+        }).then((value) => {
+          switch (value) {
+            case "Transaction":
+              window.open(`https://arbiscan.io/tx${hx.hash}`, "_blank");
+              break;
+            default:
+          }
+        });
       }
+    } catch (e) {}
 
-    });
-}
-      // swal('Create Pool Transaction is in Process', 'Please Follow Metamask ');
-    } else {
-      swal('TokenA Amount  and TokenB Amount Should be same ');
-    }
+    // swal('Create Pool Transaction is in Process', 'Please Follow Metamask ');
   }
   return (
     <>
       <Container className="h-100 pool">
         <Row>
           <Col size="12" className="pt-5">
-            <div 
-             className={`pool__container ${lightMode && 'pool__container--light'}`}
+            <div
+              className={`pool__container ${
+                lightMode && "pool__container--light"
+              }`}
             >
-            {/* <Form 
+              {/* <Form 
             
             className='shadow px-4 my-5 bg-dark'
             > */}
               <Row>
                 <Col size="12" className="my-2 create-pool-title">
-                  <H2><div className={`swap_title ${lightMode && 'swap_title--light'}`}>
-                    Create Pool
-                    </div></H2>
+                  <H2>
+                    <div
+                      className={`swap_title ${
+                        lightMode && "swap_title--light"
+                      }`}
+                    >
+                      Create Pool
+                    </div>
+                  </H2>
                 </Col>
                 <Row>
                   <Col size="1" />
@@ -211,8 +203,14 @@ if(hash){
                     <Col size="12">
                       <Col className="mb-22">
                         <H3>
-                        <div className={`swap_title ${lightMode && 'swap_title--light'}`}>
-                          Select Pair</div></H3>
+                          <div
+                            className={`swap_title ${
+                              lightMode && "swap_title--light"
+                            }`}
+                          >
+                            Select Pair
+                          </div>
+                        </H3>
                       </Col>
                       <Row>
                         <Col>
@@ -222,14 +220,14 @@ if(hash){
                             data-bs-target="#tokenModal"
                             data-bs-toggle="modal"
                             aria-expanded="false"
-                            style={{ width: '100%', borderRadius: 4 }}
+                            style={{ width: "100%", borderRadius: 4 }}
                           >
                             <div className="create-pool-tokenDropdown">
                               {tokenOne
-                                ? String(tokenOne['contract_name'])
-                                    .split(' ')[0]
+                                ? String(tokenOne["contract_name"])
+                                    .split(" ")[0]
                                     .trim()
-                                : 'Select a token'}
+                                : "Select a token"}
                             </div>
                           </StyledDropBtn>
                         </Col>
@@ -240,14 +238,14 @@ if(hash){
                             data-bs-target="#tokenModalTwo"
                             data-bs-toggle="modal"
                             aria-expanded="false"
-                            style={{ width: '100%', borderRadius: 4 }}
+                            style={{ width: "100%", borderRadius: 4 }}
                           >
                             <div className="create-pool-tokenDropdown">
                               {tokenTwo
-                                ? String(tokenTwo['symbol'])
-                                    .split(' ')[0]
+                                ? String(tokenTwo["symbol"])
+                                    .split(" ")[0]
                                     .trim()
-                                : 'Select a token'}
+                                : "Select a token"}
                             </div>
                           </StyledDropBtn>
                         </Col>
@@ -255,37 +253,49 @@ if(hash){
                     </Col>
                     <Col size="12" className="depositAmount">
                       <Col className="mb-22">
-                        <H3><div className={`mt-3 swap_title ${lightMode && 'swap_title--light'}`}>
-                          Deposit Amount</div></H3>
+                        <H3>
+                          <div
+                            className={`mt-3 swap_title ${
+                              lightMode && "swap_title--light"
+                            }`}
+                          >
+                            Deposit Amount
+                          </div>
+                        </H3>
                       </Col>
                       <Col>
                         {tokenOne ? (
                           <div className="depositAmount-group">
-                            <div  className={`selectedToken-div swap_title ${lightMode && 'swap_title--light'}`} >
+                            <div
+                              className={`selectedToken-div swap_title ${
+                                lightMode && "swap_title--light"
+                              }`}
+                            >
                               {tokenOne &&
-                                String(tokenOne['contract_name'])
-                                  .split(' ')[0]
+                                String(tokenOne["contract_name"])
+                                  .split(" ")[0]
                                   .trim()}
                             </div>
 
-                          
                             <input
-                              value={tokenOne['tokenaddress']}
+                              value={tokenOne["tokenaddress"]}
                               onChange={(e) => setTokenOne(e.target.value)}
-                              type={'hidden'}
-                              className={`from__input ${lightMode && 'from__input--light'}`}
+                              type={"hidden"}
+                              className={`from__input ${
+                                lightMode && "from__input--light"
+                              }`}
                             />
-                    
-                            
+
                             <input
                               onChange={(e) =>
                                 setTokenOneAmount(e.target.value)
                               }
                               placeholder="0.0"
-                              type={'number'}
-                              className={`from__input-pool ${lightMode && 'from__input--light-pool'}`}
+                              type={"number"}
+                              className={`from__input-pool ${
+                                lightMode && "from__input--light-pool"
+                              }`}
                             />
-                          
                           </div>
                         ) : (
                           <div className="select-a-token">Select a Token</div>
@@ -294,36 +304,44 @@ if(hash){
                       <Col>
                         {tokenTwo ? (
                           <div className="depositAmount-group">
-                            <span className={`selectedToken-div swap_title ${lightMode && 'swap_title--light'}`}>
+                            <span
+                              className={`selectedToken-div swap_title ${
+                                lightMode && "swap_title--light"
+                              }`}
+                            >
                               {tokenTwo &&
-                                String(tokenTwo['symbol']).split(' ')[0].trim()}
+                                String(tokenTwo["symbol"]).split(" ")[0].trim()}
                             </span>
-                             <div >
-                            <input
-                              value={tokenTwo['id']}
-                              onChange={(e) => setTokenTwo(e.target.value)}
-                              type={'hidden'}
-                              className={`from__input ${lightMode && 'from__input--light'}`}
-                            />
-                            <input
-                              onChange={(e) =>
-                                setTokenTwoAmount(e.target.value)
-                              }
-                              placeholder="0.0"
-                              type={'number'}
-                              className={`from__input-pool ${lightMode && 'from__input--light-pool'}`}
-                            />
-                          </div>
+                            <div>
+                              <input
+                                value={tokenTwo["id"]}
+                                onChange={(e) => setTokenTwo(e.target.value)}
+                                type={"hidden"}
+                                className={`from__input ${
+                                  lightMode && "from__input--light"
+                                }`}
+                              />
+                              <input
+                                onChange={(e) =>
+                                  setTokenTwoAmount(e.target.value)
+                                }
+                                placeholder="0.0"
+                                type={"number"}
+                                className={`from__input-pool ${
+                                  lightMode && "from__input--light-pool"
+                                }`}
+                              />
+                            </div>
                           </div>
                         ) : (
                           <div className="select-a-token">Select a Token</div>
                         )}
                       </Col>
                       <Col>
-                        {' '}
+                        {" "}
                         <Button
                           className="row justify-content-center mt-2 mb-2 btnCreatePool"
-                          label={'Create Pool'}
+                          label={"Create Pool"}
                           primary
                           onClick={handleCreatePool}
                         />
@@ -333,13 +351,12 @@ if(hash){
                   <Col size="1" />
                 </Row>
               </Row>
-            </div> 
+            </div>
             {/* </Form> */}
-            
           </Col>
         </Row>
-        <div className='mt-5'>
-        <Pools  lightMode={lightMode}/>
+        <div className="mt-5">
+          <Pools lightMode={lightMode} />
         </div>
       </Container>
       <BottomBar />
