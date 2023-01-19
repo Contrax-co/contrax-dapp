ccc350af396f6c12870dc0eae272a624a73d1a29
Abdul Rafay <abdulrafay@contrax.finance>
2023-01-19 10:59:35 +0500

    Wallet updates

--- src/context/WalletProvider.tsx
+++ src/context/WalletProvider.tsx
@@ -7,6 +7,9 @@ import { defaultChainId } from "src/config/constants";
 import { useQuery } from "@tanstack/react-query";
 import { ACCOUNT_BALANCE } from "src/config/constants/query";
 import useConstants from "src/hooks/useConstants";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
 
 interface IWalletContext {
     /**
@@ -65,22 +68,56 @@ interface IProps {
     children: React.ReactNode;
 }
 
-const WalletProvider: React.FC<IProps> = ({ children }) => {
-    const [{ wallet }, connect, disconnect, updateBalances, setWalletModules] = useConnectWallet();
-    const connectedWallets = useWallets();
-    const { NETWORK_NAME } = useConstants();
-    const [currentWallet, setCurrentWallet] = React.useState("");
-    const [provider, setProvider] = React.useState<ethers.ethers.providers.Web3Provider | undefined>(undefined);
-    const [signer, setSigner] = React.useState<ethers.ethers.providers.JsonRpcSigner | undefined>(undefined);
-    const [
-        {
-            chains, // the list of chains that web3-onboard was initialized with
-            connectedChain, // the current chain the user's wallet is connected to
-            settingChain, // boolean indicating if the chain is in the process of being set
-        },
-        setChain, // function to call to initiate user to switch chains in their wallet
-    ] = useSetChain();
+const clientId = "BNN7bsHpQ9ce3JcedpapbQ06eoYt-tu_yxrQNeH0mjJTXCwZFTClUDjEYWlxdtDP9hVngAi_609tp_M_VNVym9E";
 
+const WalletProvider: React.FC<IProps> = ({ children }) => {
+    const [web3auth, setWeb3auth] = React.useState<Web3Auth>();
+    const [provider, setProvider] = React.useState<ethers.providers.Web3Provider>();
+    const [currentWallet, setCurrentWallet] = React.useState<string>("");
+    const [networkId, setNetworkId] = React.useState<number>(defaultChainId);
+
+    const { NETWORK_NAME } = useConstants();
+    const [signer, setSigner] = React.useState<ethers.ethers.providers.JsonRpcSigner | undefined>(undefined);
+
+    React.useEffect(() => {
+        const init = async () => {
+            try {
+                const web3auth = new Web3Auth({
+                    clientId,
+                    web3AuthNetwork: "testnet", // mainnet, aqua, celeste, cyan or testnet
+                    chainConfig: {
+                        chainNamespace: CHAIN_NAMESPACES.EIP155,
+                        chainId: "0xA4B1",
+                        rpcTarget: "https://arb1.arbitrum.io/rpc", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+                    },
+                });
+
+                const openloginAdapter = new OpenloginAdapter({
+                    loginSettings: {
+                        mfaLevel: "none", // Pass on the mfa level of your choice: default, optional, mandatory, none
+                    },
+                });
+                web3auth.configureAdapter(openloginAdapter);
+
+                setWeb3auth(web3auth);
+
+                await web3auth.initModal();
+
+                if (web3auth.provider) {
+                    const _provider = new ethers.providers.Web3Provider(web3auth.provider);
+                    setProvider(_provider);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        init();
+    }, []);
+    console.log("provider", provider);
+    console.log("signer", signer);
+    console.log("wallet", currentWallet);
+    console.log("networkId", networkId);
     const getBalance = async () => {
         const balance = await provider?.getBalance(currentWallet);
         const formattedBal = Number(ethers.utils.formatUnits(balance || 0, 18));
@@ -88,49 +125,40 @@ const WalletProvider: React.FC<IProps> = ({ children }) => {
     };
 
     const connectWallet = async () => {
-        const wallets = await connect();
-        // TODO: Remove set user session
-        if (wallets) {
-            setUserSession({
-                address: wallets[0].accounts[0].address,
-                networkId: wallets[0].chains[0].id,
-            });
-
-            setCurrentWallet(wallets[0].accounts[0].address);
+        const _provider = await web3auth?.connect();
+        if (_provider) {
+            setProvider(new ethers.providers.Web3Provider(_provider));
         }
     };
 
-    async function network() {
-        const chainId = 42161;
-        if (!connectedChain?.id) return;
-        if (connectedChain.id !== chainId.toString()) {
-            try {
-                await setChain({
-                    chainId: chainId.toString(),
-                });
-            } catch (err: any) {
-                // This error code indicates that the chain has not been added to MetaMask
-                if (err.code === 4902) {
-                    await wallet?.provider?.request({
-                        method: "wallet_addEthereumChain",
-                        params: [
-                            {
-                                chainName: "Arbitrum One",
-                                chainId: "0xA4B1",
-                                nativeCurrency: { name: "ETH", decimals: 18, symbol: "ETH" },
-                                rpcUrls: ["https://arb1.arbitrum.io/rpc/"],
-                            },
-                        ],
-                    });
-                }
-            }
-        }
-    }
+    // async function network() {
+    //     const chainId = 42161;
+    //     if (!connectedChain?.id) return;
+    //     if (connectedChain.id !== chainId.toString()) {
+    //         try {
+    //             web3auth.
+    //         } catch (err: any) {
+    //             // This error code indicates that the chain has not been added to MetaMask
+    //             if (err.code === 4902) {
+    //                 await wallet?.provider?.request({
+    //                     method: "wallet_addEthereumChain",
+    //                     params: [
+    //                         {
+    //                             chainName: "Arbitrum One",
+    //                             chainId: "0xA4B1",
+    //                             nativeCurrency: { name: "ETH", decimals: 18, symbol: "ETH" },
+    //                             rpcUrls: ["https://arb1.arbitrum.io/rpc/"],
+    //                         },
+    //                     ],
+    //                 });
+    //             }
+    //         }
+    //     }
+    // }
 
     async function logout() {
-        removeUserSession();
-        setCurrentWallet("");
-        if (wallet) disconnect(wallet);
+        await web3auth?.logout();
+        setProvider(undefined);
     }
 
     const displayAccount = React.useMemo(
@@ -138,43 +166,33 @@ const WalletProvider: React.FC<IProps> = ({ children }) => {
         [currentWallet]
     );
 
-    React.useEffect(() => {
-        network();
-    }, [connectedChain, wallet, provider]);
+    // React.useEffect(() => {
+    //     // network();
+    // }, [networkId]);
 
     React.useEffect(() => {
-        if (wallet) {
-            const provider = new ethers.providers.Web3Provider(wallet.provider, "any");
-            setProvider(provider);
-            provider.send("eth_requestAccounts", []).then(() => {
-                const signer = provider.getSigner();
-                setSigner(signer);
+        if (provider) {
+            console.log("geting sigener");
+            const _signer = provider.getSigner();
+            setSigner(_signer);
+
+            provider.getNetwork().then((networkDetails) => {
+                setNetworkId(networkDetails.chainId);
             });
-            setCurrentWallet(wallet.accounts[0].address);
         } else {
-            setCurrentWallet("");
             setSigner(undefined);
         }
-    }, [wallet]);
+    }, [provider]);
 
     React.useEffect(() => {
-        if (!connectedWallets.length) return;
-
-        const connectedWalletsLabelArray = connectedWallets.map(({ label }) => label);
-        localStorage.setItem("connectedWallets", JSON.stringify(connectedWalletsLabelArray));
-    }, [connectedWallets, wallet]);
-
-    React.useEffect(() => {
-        const previouslyConnectedWallets = JSON.parse(localStorage.getItem("connectedWallets") as string);
-
-        if (previouslyConnectedWallets?.length) {
-            connect({
-                autoSelect: previouslyConnectedWallets[0],
-            }).then((walletConnected) => {
-                console.log("connected wallets: ", walletConnected);
+        if (signer)
+            signer.getAddress().then((address) => {
+                setCurrentWallet(address);
             });
+        else {
+            setCurrentWallet("");
         }
-    }, [connect]);
+    }, [signer]);
 
     const { data: balance, refetch: refetchBalance } = useQuery(
         ACCOUNT_BALANCE(currentWallet, currentWallet, NETWORK_NAME),
@@ -191,7 +209,7 @@ const WalletProvider: React.FC<IProps> = ({ children }) => {
             value={{
                 currentWallet,
                 connectWallet,
-                networkId: parseInt(connectedChain?.id || "0", 16),
+                networkId,
                 logout,
                 displayAccount,
                 signer,
