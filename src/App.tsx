f481fd44e568f39151735e4107202f3d98ae1556
Abdul Rafay <abdulrafay@contrax.finance>
2023-01-30 18:09:47 +0500

    Updates Apy, wallet etc

--- src/App.tsx
+++ src/App.tsx
@@ -8,56 +8,31 @@ import CreatePool from "src/pages/CreatePool/CreatePool";
 import Dashboard from "src/pages/Dashboard/Dashboard";
 import AppProvider from "src/context/AppProvider";
 import "react-tooltip/dist/react-tooltip.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Web3OnboardProvider } from "@web3-onboard/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Exchange from "./pages/Exchange/Exchange";
 import "./styles/global.scss";
-import { WagmiConfig } from "wagmi";
-import { wagmiClient, chains } from "./config/walletConfig";
-import "@rainbow-me/rainbowkit/styles.css";
-import Logo from "src/assets/images/logo.png";
-
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import WalletDisclaimer from "./components/WalletDisclaimer/WalletDisclaimer";
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-        },
-    },
-});
 
 function App() {
     return (
-        <QueryClientProvider client={queryClient}>
-            <WagmiConfig client={wagmiClient}>
-                <RainbowKitProvider
-                    chains={chains}
-                    showRecentTransactions={false}
-                    appInfo={{ appName: "Contrax", disclaimer: WalletDisclaimer }}
-                >
-                    <WalletProvider>
-                        <AppProvider>
-                            <Router>
-                                <Routes>
-                                    <Route path="/" element={<Home />}>
-                                        <Route path="" element={<Dashboard />} />
-                                        <Route path="/farms" element={<Farms />} />
-                                        <Route path="/exchange" element={<Exchange />} />
-                                        <Route path="create-token" element={<CreateToken />} />
-                                        <Route path="create-pool" element={<CreatePool />} />
-                                        <Route path="*" element={<h3 style={{ color: "white" }}>Not Found</h3>} />
-                                    </Route>
-                                </Routes>
-                            </Router>
-                        </AppProvider>
-                    </WalletProvider>
-                    <ReactQueryDevtools />
-                </RainbowKitProvider>
-            </WagmiConfig>
-        </QueryClientProvider>
+        <>
+            <WalletProvider>
+                <AppProvider>
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<Home />}>
+                                <Route path="" element={<Dashboard />} />
+                                <Route path="/farms" element={<Farms />} />
+                                <Route path="/exchange" element={<Exchange />} />
+                                <Route path="create-token" element={<CreateToken />} />
+                                <Route path="create-pool" element={<CreatePool />} />
+                                <Route path="*" element={<h3 style={{ color: "white" }}>Not Found</h3>} />
+                            </Route>
+                        </Routes>
+                    </Router>
+                </AppProvider>
+            </WalletProvider>
+            <ReactQueryDevtools />
+        </>
     );
 }
 
