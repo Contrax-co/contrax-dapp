a29429acf0c740d8433475f2545b25662fd9bfc5
Abdul Rafay <abdulrafay@contrax.finance>
2023-01-30 18:56:30 +0500

    Web3auth client id update

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -14,4 +14,6 @@ export const COVALENT_API_TOKEN = process.env.REACT_APP_COVALENT_API_KEY;
 export const COVALENT_HQ_URL = "https://api.covalenthq.com/v1";
 export const APY_VISION_URL = "https://api.apy.vision";
 export const APY_VISION_STATS_URL = "https://stats.apy.vision/api/v1";
-export const WEB3AUTH_CLIENT_ID = process.env.REACT_APP_WEB3AUTH_CLIENT_ID;
+// export const WEB3AUTH_CLIENT_ID = process.env.REACT_APP_WEB3AUTH_CLIENT_ID;
+export const WEB3AUTH_CLIENT_ID =
+    "BNN7bsHpQ9ce3JcedpapbQ06eoYt-tu_yxrQNeH0mjJTXCwZFTClUDjEYWlxdtDP9hVngAi_609tp_M_VNVym9E";
