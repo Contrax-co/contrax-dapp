5a8a3b627a59f3816bfd9e9776d0b879d52ed0a1
Abdul Rafay <abdulrafay@contrax.finance>
2023-02-03 13:21:45 +0500

    undefined...undefined bug fix dashboard

--- src/context/WalletProvider.tsx
+++ src/context/WalletProvider.tsx
@@ -90,65 +90,12 @@ const WalletProvider: React.FC<IProps> = ({ children }) => {
 
     const { data: signer } = useSigner();
 
-    // const {setOpen} = useModal()
     const { address: currentWallet } = useAccount();
     const { disconnect } = useDisconnect();
     const { connectors } = useConnect();
     const [networkId, setNetworkId] = React.useState<number>(defaultChainId);
     const { NETWORK_NAME } = useConstants();
     const { openConnectModal } = useConnectModal();
-
-    // React.useEffect(() => {
-    //     const init = async () => {
-    //         try {
-    //             const web3auth = new Web3Auth({
-    //                 clientId,
-    //                 web3AuthNetwork: "testnet", // mainnet, aqua, celeste, cyan or testnet
-    //                 chainConfig: {
-    //                     chainNamespace: CHAIN_NAMESPACES.EIP155,
-    //                     chainId: "0xA4B1",
-    //                     rpcTarget: "https://arb1.arbitrum.io/rpc", // This is the public RPC we have added, please pass on your own endpoint while creating an app
-    //                     blockExplorer: "https://arbiscan.io/",
-    //                     displayName: "Arbitrum One",
-    //                     ticker: "ETH",
-    //                     tickerName: "Ether",
-    //                 },
-    //             });
-    //             const metamaskAdapter = new MetamaskAdapter({
-    //                 sessionTime: 3600, // 1 hour in seconds
-    //             });
-
-    //             const openloginAdapter = new OpenloginAdapter({
-    //                 loginSettings: {
-    //                     mfaLevel: "none", // Pass on the mfa level of your choice: default, optional, mandatory, none
-    //                 },
-    //             });
-    //             const walletConnectV1Adapter = new WalletConnectV1Adapter({
-    //                 adapterSettings: {
-    //                     bridge: "https://bridge.walletconnect.org",
-    //                 },
-    //                 sessionTime: 3600, // 1 day in seconds
-    //             });
-
-    //             web3auth.configureAdapter(walletConnectV1Adapter);
-    //             web3auth.configureAdapter(openloginAdapter);
-    //             web3auth.configureAdapter(metamaskAdapter);
-
-    //             setWeb3auth(web3auth);
-
-    //             await web3auth.initModal();
-
-    //             if (web3auth.provider) {
-    //                 const _provider = new ethers.providers.Web3Provider(web3auth.provider);
-    //                 setProvider(_provider);
-    //             }
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     };
-
-    //     init();
-    // }, []);
 
     const getBalance = async () => {
         if (!provider || !currentWallet) return ethers.BigNumber.from(0);
@@ -160,49 +107,19 @@ const WalletProvider: React.FC<IProps> = ({ children }) => {
         if (openConnectModal) openConnectModal();
 
         return false;
-        // await connectAsync({
-        //     connector: connectors[0],
-        //     chainId: defaultChainId,
-        // });
     };
-
-    // async function network() {
-    //     const chainId = 42161;
-    //     if (!connectedChain?.id) return;
-    //     if (connectedChain.id !== chainId.toString()) {
-    //         try {
-    //             web3auth.
-    //         } catch (err: any) {
-    //             // This error code indicates that the chain has not been added to MetaMask
-    //             if (err.code === 4902) {
-    //                 await wallet?.provider?.request({
-    //                     method: "wallet_addEthereumChain",
-    //                     params: [
-    //                         {
-    //                             chainName: "Arbitrum One",
-    //                             chainId: "0xA4B1",
-    //                             nativeCurrency: { name: "ETH", decimals: 18, symbol: "ETH" },
-    //                             rpcUrls: ["https://arb1.arbitrum.io/rpc/"],
-    //                         },
-    //                     ],
-    //                 });
-    //             }
-    //         }
-    //     }
-    // }
 
     async function logout() {
         disconnect();
     }
 
     const displayAccount = React.useMemo(
-        () => `${currentWallet?.substring(0, 6)}...${currentWallet?.substring(currentWallet.length - 5)}`,
+        () =>
+            currentWallet
+                ? `${currentWallet?.substring(0, 6)}...${currentWallet?.substring(currentWallet.length - 5)}`
+                : "",
         [currentWallet]
     );
-
-    // React.useEffect(() => {
-    //     network();
-    // }, [connectedChain, wallet, provider]);
 
     const { data: balanceBigNumber, refetch: refetchBalance } = useQuery(
         ACCOUNT_BALANCE(currentWallet!, currentWallet!, NETWORK_NAME),
