b1f9f392d7422bd6cda135b209d887f777b612a1
Abdul Rafay <abdulrafay@contrax.finance>
2023-02-08 16:20:15 +0500

    Change network to arbitrum message added in vaults section

--- src/pages/Dashboard/JoinedVaults/Vaults.tsx
+++ src/pages/Dashboard/JoinedVaults/Vaults.tsx
@@ -1,17 +1,26 @@
 import VaultItem from "./VaultItem";
 import "./Vaults.css";
 import useVaults from "src/hooks/vaults/useVaults";
+import useWallet from "src/hooks/useWallet";
+import { defaultChainId } from "src/config/constants";
+import useApp from "src/hooks/useApp";
 
 interface Props {}
 
 const Vaults: React.FC<Props> = () => {
+    const { lightMode } = useApp();
     const { vaults } = useVaults();
+    const { networkId } = useWallet();
 
     return (
-        <div className={`vaults_container`}>
-            {vaults.map((vault) => (
-                <VaultItem vault={vault} key={vault.id} />
-            ))}
+        <div className={`vaults_container`} style={networkId === defaultChainId ? undefined : { display: "block" }}>
+            {networkId === defaultChainId ? (
+                vaults.map((vault) => <VaultItem vault={vault} key={vault.id} />)
+            ) : (
+                <div className={`change_network_section ${lightMode && "change_network_section_light"}`}>
+                    <p>Please change network to Arbitrum to use the farms</p>
+                </div>
+            )}
         </div>
     );
 };
