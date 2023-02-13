b7b0bf2d3e8d46b29b0cbb087a4e8e45e5b7e10b
Abdul Rafay <abdulrafay@contrax.finance>
2023-02-13 11:26:05 +0500

    Light theme for TokenBalance component

--- src/pages/Dashboard/TokenBalances/TokenBalances.tsx
+++ src/pages/Dashboard/TokenBalances/TokenBalances.tsx
@@ -1,17 +1,19 @@
 import { ethers } from "ethers";
 import { FC } from "react";
+import useApp from "src/hooks/useApp";
 import { useTokens } from "src/hooks/useTokens";
 import styles from "./TokenBalances.module.scss";
 
 interface IProps {}
 
 export const TokenBalances: FC<IProps> = (props) => {
+    const { lightMode } = useApp();
     const { tokens } = useTokens();
     return (
         <div className={styles.container}>
             {tokens.map((token) =>
                 Number(token.balance) > 0.01 ? (
-                    <div className={styles.tokenCard}>
+                    <div className={`${styles.tokenCard} ${lightMode && styles.tokenCardLight}`}>
                         <img className={styles.tokenLogo} src={token.logo} alt="logo" />
                         <div className={styles.tokenDesription}>
                             <p className={styles.name}>{token.name}</p>
