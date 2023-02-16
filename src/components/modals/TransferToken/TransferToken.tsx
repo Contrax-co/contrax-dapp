0124fc1c648e1e4ae8552f7b8157d09beb7213e1
Abdul Rafay <abdulrafay@contrax.finance>
2023-02-16 18:01:05 +0500

    conflict resolved

--- src/components/modals/TransferToken/TransferToken.tsx
+++ src/components/modals/TransferToken/TransferToken.tsx
@@ -52,54 +52,49 @@ export const TransferToken: FC<IProps> = ({ token, setSelectedToken, refetchBala
     };
 
     return (
-        <div className={styles.backdrop} onClick={() => setSelectedToken(undefined)}>
-            <div
-                className={`${styles.container} ${lightMode && styles.container_light}`}
-                onClick={(e) => e.stopPropagation()}
-            >
-                <form className={styles.transferForm} onSubmit={handleSubmit}>
-                    <h1 className={styles.heading}>Transfer {token.name}</h1>
-                    <div className={styles.row}>
-                        <label htmlFor="reciverAddress" className={styles.label}>
-                            Send To:
-                        </label>
-                        <input
-                            className={`${styles.inputs} ${lightMode && styles.inputs_light}`}
-                            type="text"
-                            id="reciverAddress"
-                            placeholder="Reciver Address"
-                            value={reciverAddress}
-                            onChange={(e) => setReciverAddress(e.target.value)}
-                        />
-                    </div>
-                    <div className={styles.row}>
-                        <label htmlFor="amount" className={styles.label}>
-                            Amount:
-                        </label>
-                        <input
-                            className={`${styles.inputs} ${lightMode && styles.inputs_light}`}
-                            type="number"
-                            id="amount"
-                            placeholder="e.g. 250"
-                            value={amount}
-                            onChange={(e) => {
-                                setAmount(Number(e.target.value));
-                                setMax(false);
-                            }}
-                        />
-                        <button type="button" className={styles.maxButton} onClick={handleMaxClick}>
-                            MAX
-                        </button>
-                    </div>
-                    <button
-                        type="submit"
-                        disabled={isLoading || amount <= 0 || !reciverAddress}
-                        className={`${styles.button} ${lightMode && styles.button_light}`}
-                    >
-                        Transfer
+        <ModalLayout onClose={() => setSelectedToken(undefined)}>
+            <form className={styles.transferForm} onSubmit={handleSubmit}>
+                <h1>Transfer {token.name}</h1>
+                <div className={styles.row}>
+                    <label htmlFor="reciverAddress" className={styles.label}>
+                        Send To:
+                    </label>
+                    <input
+                        className={`${styles.inputs} ${lightMode && styles.inputs_light}`}
+                        type="text"
+                        id="reciverAddress"
+                        placeholder="Reciver Address"
+                        value={reciverAddress}
+                        onChange={(e) => setReciverAddress(e.target.value)}
+                    />
+                </div>
+                <div className={styles.row}>
+                    <label htmlFor="amount" className={styles.label}>
+                        Amount:
+                    </label>
+                    <input
+                        className={`${styles.inputs} ${lightMode && styles.inputs_light}`}
+                        type="number"
+                        id="amount"
+                        placeholder="e.g. 250"
+                        value={amount}
+                        onChange={(e) => {
+                            setAmount(Number(e.target.value));
+                            setMax(false);
+                        }}
+                    />
+                    <button type="button" className={styles.maxButton} onClick={handleMaxClick}>
+                        MAX
                     </button>
-                </form>
-            </div>
-        </div>
+                </div>
+                <button
+                    type="submit"
+                    disabled={isLoading || amount <= 0 || !reciverAddress}
+                    className={`${styles.button} ${lightMode && styles.button_light}`}
+                >
+                    Transfer
+                </button>
+            </form>
+        </ModalLayout>
     );
 };
