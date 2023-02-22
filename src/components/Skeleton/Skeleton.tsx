eb313a28b531663b5235b22980a63a116edd0078
Abdul Rafay <abdulrafay@contrax.finance>
2023-02-22 17:12:37 +0500

    Detail input skeleton

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, CSSProperties } from "react";
 import useApp from "src/hooks/useApp";
 import styles from "./Skeleton.module.scss";
 
@@ -7,18 +7,21 @@ interface IProps {
     h: number | string;
     bRadius?: number;
     bg?: string;
+    style?: CSSProperties;
+    className?: string;
 }
 
-export const Skeleton: FC<IProps> = ({ w, h, bg, bRadius = 5 }) => {
+export const Skeleton: FC<IProps> = ({ w, h, bg, bRadius = 5, style, className }) => {
     const { lightMode } = useApp();
     return (
         <div
-            className={styles.container}
+            className={`${styles.container} ${className}`}
             style={{
                 width: w,
                 height: h,
                 background: bg || (lightMode ? "#f5f6f9" : "#001428"),
                 borderRadius: bRadius,
+                ...style,
             }}
         ></div>
     );
