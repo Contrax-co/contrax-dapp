bae444f00d306f3537be5ddeff394486b3f251eb
Abdul Rafay <abdulrafay@contrax.finance>
2023-02-28 17:02:16 +0500

    vaults with stake value of less than 1 cents removed

--- src/hooks/useVaults.ts
+++ src/hooks/useVaults.ts
@@ -4,7 +4,7 @@ import { useFarmDetails } from "./farms/useFarms";
 export const useVaults = () => {
     const { farmDetails, isLoading } = useFarmDetails();
     const vaults = useMemo(
-        () => farmDetails.filter((farm) => farm.userVaultBalance * farm.priceOfSingleToken >= 0.01),
+        () => farmDetails.filter((farm) => farm.userVaultBalance * farm.priceOfSingleToken > 0.01),
         [farmDetails]
     );
     return { vaults, isLoading };
