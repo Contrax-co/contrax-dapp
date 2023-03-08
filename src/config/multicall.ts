69626e99689cac516769cc06b64439ff9ee851ae
Abdul Rafay <abdulrafay@contrax.finance>
2023-03-08 16:37:31 +0500

    Multicall config update

--- src/config/multicall.ts
+++ src/config/multicall.ts
@@ -1,11 +1,7 @@
 import { Multicall, providers } from "@0xsequence/multicall";
 import ethers from "ethers";
 
-export const multicall = new Multicall({
-    timeWindow: 500, // 1second
-    verbose: true,
-    batchSize: 100,
-});
+export const multicall = new Multicall();
 Multicall.DefaultOptions.verbose = true;
 Multicall.DefaultOptions.timeWindow = 500;
 Multicall.DefaultOptions.batchSize = 100;
