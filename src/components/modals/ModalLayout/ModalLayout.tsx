330f75387e9fe87bd7c1a414f31fae3d6c0acce7
Abdul Rafay <abdulrafay@contrax.finance>
2023-03-24 13:12:45 +0500

    modal layout component added custom classes

--- src/components/modals/ModalLayout/ModalLayout.tsx
+++ src/components/modals/ModalLayout/ModalLayout.tsx
@@ -6,15 +6,16 @@ interface IProps {
     onClose: Function;
     children: ReactNode;
     style?: any;
+    className?: string;
 }
 
-export const ModalLayout: FC<IProps> = ({ onClose, children, style }) => {
+export const ModalLayout: FC<IProps> = ({ onClose, children, style, className }) => {
     const { lightMode } = useApp();
 
     return (
         <div className={styles.backdrop} onClick={(e) => onClose(e)}>
             <div
-                className={`${styles.container} ${lightMode && styles.container_light}`}
+                className={`${styles.container} ${lightMode && styles.container_light} ${className}`}
                 onClick={(e) => e.stopPropagation()}
                 style={style}
             >
