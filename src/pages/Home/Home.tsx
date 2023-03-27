a51926bd470ae5b2fa486b031e043d5ecc5502aa
Abdul Rafay <rafay@contrax.finance>
2023-03-27 16:17:39 +0500

    code standarization, useAppSelector instead of useSelector

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,15 +4,14 @@ import TopBar from "src/components/Topbar/TopBar";
 import { Outlet } from "react-router-dom";
 import useApp from "src/hooks/useApp";
 import { MdCancel } from "react-icons/md";
-import { useSelector } from "react-redux";
-import { RootState } from "src/state";
+import { useAppSelector } from "src/state";
 import "./Home.css";
 import { Maintainance } from "src/components/modals/MaintainanceModal/Maintainance";
 
 function Home() {
     const { lightMode, supportChat, toggleSupportChat } = useApp();
     const [openBurgerMenu, setOpenBurgerMenu] = useState(false);
-    const isError = useSelector((state: RootState) => state.error.isError);
+    const isError = useAppSelector((state) => state.error.isError);
     return (
         <div className={`page ${lightMode && "page--light"}`}>
             <div className="ac_page">
