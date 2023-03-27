986f7619b6cf4c973b83b639e2c8829235829c94
Abdul Rafay <rafay@contrax.finance>
2023-03-27 15:14:14 +0500

    Maintenance Modal added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Sidebar from "src/components/Sidebar/Sidebar";
 import TopBar from "src/components/Topbar/TopBar";
-import "./Home.css";
 import { Outlet } from "react-router-dom";
 import useApp from "src/hooks/useApp";
 import { MdCancel } from "react-icons/md";
+import { useSelector } from "react-redux";
+import { RootState } from "src/state";
+import "./Home.css";
+import { Maintainance } from "src/components/modals/MaintainanceModal/Maintainance";
 
 function Home() {
     const { lightMode, supportChat, toggleSupportChat } = useApp();
     const [openBurgerMenu, setOpenBurgerMenu] = useState(false);
+    const isError = useSelector((state: RootState) => state.error.isError);
     return (
         <div className={`page ${lightMode && "page--light"}`}>
             <div className="ac_page">
@@ -35,6 +39,7 @@ function Home() {
                     )}
                 </div>
             </div>
+            {isError && <Maintainance />}
         </div>
     );
 }
