d90e9992b86796d3717e2aa502b5e6287d595e7b
rafay-contrax <rafay@contrax.finance>
2023-04-03 13:18:22 +0500

    Apy not loaded show "--"

--- src/pages/Dashboard/JoinedVaults/VaultItem.tsx
+++ src/pages/Dashboard/JoinedVaults/VaultItem.tsx
@@ -101,7 +101,13 @@ const VaultItem: React.FC<Props> = ({ vault }) => {
 
                             <div className={`vault_items_bottom_categories`}>
                                 <p className={`vault_items_title ${lightMode && "vault_items_title--light"}`}>APY</p>
-                                <p>{apy < 0.01 ? apy.toPrecision(2).slice(0, -1) : toFixedFloor(apy, 2).toString()}%</p>
+                                <p>
+                                    {toFixedFloor(apy || 0, 2) == 0
+                                        ? "--"
+                                        : apy < 0.01
+                                        ? `${apy.toPrecision(2).slice(0, -1)}%`
+                                        : `${toFixedFloor(apy, 2).toString()}%`}
+                                </p>
                             </div>
                             {/* <div className={`vault_items_bottom_categories`}>
                                 <p className={`vault_items_title ${lightMode && "vault_items_title--light"}`}>Earned</p>
