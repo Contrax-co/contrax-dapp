7d2f6cf526ce2a04ba641f894ed0d9eb6966413e
rafay-contrax <rafay@contrax.finance>
2023-04-03 13:36:50 +0500

    APY Error handling

--- src/api/apy.ts
+++ src/api/apy.ts
@@ -45,9 +45,10 @@ interface ChefResponse {
 }
 
 const getSushiswapApy = async (pairAddress: string, chainId: number, provider: providers.Provider) => {
-    const priceOfSushi = await getPrice(addressesByChainId[chainId].sushiAddress, chainId);
+    try {
+        const priceOfSushi = await getPrice(addressesByChainId[chainId].sushiAddress, chainId);
 
-    let query = `{
+        let query = `{
         pair(id: "${pairAddress}") {
           name
           liquidityUSD
@@ -56,9 +57,9 @@ const getSushiswapApy = async (pairAddress: string, chainId: number, provider: p
           id
         }
       }`;
-    let res = await axios.post(SUSHUISWAP_GRAPH_URL, { query });
-    let pairData: GraphResponse = res.data.data.pair;
-    query = ` {
+        let res = await axios.post(SUSHUISWAP_GRAPH_URL, { query });
+        let pairData: GraphResponse = res.data.data.pair;
+        query = ` {
               miniChefs {
                 id
                 sushi
@@ -76,57 +77,67 @@ const getSushiswapApy = async (pairAddress: string, chainId: number, provider: p
                 }
               }
             }`;
-    res = await axios.post(SHUSHISWAP_CHEF_GRAPH_URL, { query });
-    const chefData: ChefResponse = res.data.data.miniChefs[0];
+        res = await axios.post(SHUSHISWAP_CHEF_GRAPH_URL, { query });
+        const chefData: ChefResponse = res.data.data.miniChefs[0];
 
-    let obj = {
-        allocPoint: chefData.pools[0] ? Number(chefData.pools[0].allocPoint) : 0,
-        totalAllocPoint: chefData ? Number(chefData.totalAllocPoint) : 0,
-        sushiPerSecond: chefData ? BigNumber.from(chefData.sushiPerSecond) : BigNumber.from(0),
-        sushiPerDay: chefData ? BigNumber.from(chefData.sushiPerSecond).mul(60).mul(60).mul(24) : BigNumber.from(0),
-        feeApr: Number(pairData.apr) * 100,
-        liquidityUSD: Number(pairData.liquidityUSD),
-    };
-    const sushiRewardPerDay = obj.sushiPerDay;
-    const sushiRewardPerYear = sushiRewardPerDay.mul(365);
+        let obj = {
+            allocPoint: chefData.pools[0] ? Number(chefData.pools[0].allocPoint) : 0,
+            totalAllocPoint: chefData ? Number(chefData.totalAllocPoint) : 0,
+            sushiPerSecond: chefData ? BigNumber.from(chefData.sushiPerSecond) : BigNumber.from(0),
+            sushiPerDay: chefData ? BigNumber.from(chefData.sushiPerSecond).mul(60).mul(60).mul(24) : BigNumber.from(0),
+            feeApr: Number(pairData.apr) * 100,
+            liquidityUSD: Number(pairData.liquidityUSD),
+        };
+        const sushiRewardPerDay = obj.sushiPerDay;
+        const sushiRewardPerYear = sushiRewardPerDay.mul(365);
 
-    const sushiRewardPerYearUSD =
-        (Number(toEth(sushiRewardPerYear.toString())) * priceOfSushi * obj.allocPoint) / obj.totalAllocPoint;
-    let rewardsApr = (sushiRewardPerYearUSD / obj.liquidityUSD) * 100;
-    if (chefData.pools[0] && Number(chefData.pools[0].rewarder.id) !== 0) {
-        const rewarder = chefData.pools[0].rewarder;
-        const rewardTokenContract = new Contract(pairAddress, erc20ABI, provider);
-        const rewardTokenPrice = await getPrice(rewarder.rewardToken, chainId);
-        const [balance, decimals, totalSupply] = await Promise.all([
-            rewardTokenContract.balanceOf(chefData.id),
-            rewardTokenContract.decimals(),
-            rewardTokenContract.totalSupply(),
-        ]);
-        const stakedLiquidityUSD =
-            (Number(toEth(balance, decimals)) * obj.liquidityUSD) / Number(toEth(totalSupply, decimals));
-        const rewardPerSecond = BigNumber.from(rewarder.rewardPerSecond);
-        const rewardPerDay = rewardPerSecond.mul(60).mul(60).mul(24);
-        const rewardPerYear = rewardPerDay.mul(365);
-        const rewardPerYearUSD = Number(toEth(rewardPerYear.toString(), decimals)) * rewardTokenPrice;
-        const rewarderApr = (rewardPerYearUSD / stakedLiquidityUSD) * 100;
-        rewardsApr += rewarderApr;
+        const sushiRewardPerYearUSD =
+            (Number(toEth(sushiRewardPerYear.toString())) * priceOfSushi * obj.allocPoint) / obj.totalAllocPoint;
+        let rewardsApr = (sushiRewardPerYearUSD / obj.liquidityUSD) * 100;
+        if (chefData.pools[0] && Number(chefData.pools[0].rewarder.id) !== 0) {
+            const rewarder = chefData.pools[0].rewarder;
+            const rewardTokenContract = new Contract(pairAddress, erc20ABI, provider);
+            const rewardTokenPrice = await getPrice(rewarder.rewardToken, chainId);
+            const [balance, decimals, totalSupply] = await Promise.all([
+                rewardTokenContract.balanceOf(chefData.id),
+                rewardTokenContract.decimals(),
+                rewardTokenContract.totalSupply(),
+            ]);
+            const stakedLiquidityUSD =
+                (Number(toEth(balance, decimals)) * obj.liquidityUSD) / Number(toEth(totalSupply, decimals));
+            const rewardPerSecond = BigNumber.from(rewarder.rewardPerSecond);
+            const rewardPerDay = rewardPerSecond.mul(60).mul(60).mul(24);
+            const rewardPerYear = rewardPerDay.mul(365);
+            const rewardPerYearUSD = Number(toEth(rewardPerYear.toString(), decimals)) * rewardTokenPrice;
+            const rewarderApr = (rewardPerYearUSD / stakedLiquidityUSD) * 100;
+            rewardsApr += rewarderApr;
+        }
+        const feeApr = obj.feeApr;
+        const compounding = calcCompoundingApy(rewardsApr);
+
+        const apy = feeApr + compounding + rewardsApr; // RewardsApr is included in compounding
+
+        return {
+            feeApr,
+            rewardsApr,
+            apy,
+            compounding,
+        };
+    } catch (e) {
+        console.error(e);
+        return {
+            feeApr: 0,
+            rewardsApr: 0,
+            apy: 0,
+            compounding: 0,
+        };
     }
-    const feeApr = obj.feeApr;
-    const compounding = calcCompoundingApy(rewardsApr);
-
-    const apy = feeApr + compounding + rewardsApr; // RewardsApr is included in compounding
-
-    return {
-        feeApr,
-        rewardsApr,
-        apy,
-        compounding,
-    };
 };
 
 const getSwapFishApy = async (pairAddress: string, chainId: number, provider: providers.Provider, poolId: number) => {
-    let query = `{
-        pair(id: "${pairAddress.toLowerCase()}") {
+    try {
+        let query = `{
+            pair(id: "${pairAddress.toLowerCase()}") {
             name
           liquidityUSD
           apr
@@ -134,45 +145,54 @@ const getSwapFishApy = async (pairAddress: string, chainId: number, provider: pr
           id
         }
       }`;
-    let res = await axios.post(SWAPFISH_GRAPH_URL, { query });
-    let pairData: GraphResponse = res.data.data.pair;
-    const masterChefAddress = addressesByChainId[chainId].swapfishMasterChef!;
-    const masterChefContract = new Contract(masterChefAddress, swapFishMaterchef, provider);
+        let res = await axios.post(SWAPFISH_GRAPH_URL, { query });
+        let pairData: GraphResponse = res.data.data.pair;
+        const masterChefAddress = addressesByChainId[chainId].swapfishMasterChef!;
+        const masterChefContract = new Contract(masterChefAddress, swapFishMaterchef, provider);
 
-    const [totalAllocPoint, { allocPoint }, cakeAddress, cakePerSecond] = await Promise.all([
-        masterChefContract.totalAllocPoint(),
-        masterChefContract.poolInfo(poolId),
-        masterChefContract.cake(),
-        masterChefContract.cakePerSecond(),
-    ]);
+        const [totalAllocPoint, { allocPoint }, cakeAddress, cakePerSecond] = await Promise.all([
+            masterChefContract.totalAllocPoint(),
+            masterChefContract.poolInfo(poolId),
+            masterChefContract.cake(),
+            masterChefContract.cakePerSecond(),
+        ]);
 
-    const cakePrice = await getPrice(cakeAddress, chainId);
-    const cakePerYear = cakePerSecond.mul(60).mul(60).mul(25).mul(365);
-    const cakePerYearUsd =
-        (Number(toEth(cakePerYear)) * cakePrice * allocPoint.toNumber()) / totalAllocPoint.toNumber();
-    let rewardsApr = (cakePerYearUsd / Number(pairData.liquidityUSD)) * 100;
+        const cakePrice = await getPrice(cakeAddress, chainId);
+        const cakePerYear = cakePerSecond.mul(60).mul(60).mul(25).mul(365);
+        const cakePerYearUsd =
+            (Number(toEth(cakePerYear)) * cakePrice * allocPoint.toNumber()) / totalAllocPoint.toNumber();
+        let rewardsApr = (cakePerYearUsd / Number(pairData.liquidityUSD)) * 100;
 
-    if (pairAddress === "0x78d9B037Fb873AfCf4e3E466aDfAfa8A5258CdaD") {
-        // Swapfish USDC-AGEUR liquidity pool liquidity is coming half from theGraph so we have to divide the rewards by 2 to account for half liquidity value coming from theGraph
-        rewardsApr /= 2;
+        if (pairAddress === "0x78d9B037Fb873AfCf4e3E466aDfAfa8A5258CdaD") {
+            // Swapfish USDC-AGEUR liquidity pool liquidity is coming half from theGraph so we have to divide the rewards by 2 to account for half liquidity value coming from theGraph
+            rewardsApr /= 2;
+        }
+        // const cakePerYearUsd = cakePerYear.mul(cakePrice).mul(allocPoint).div(totalAllocPoint);
+        // let rewardsApr = Number(toEth(cakePerYearUsd.div(pairData.liquidityUSD).mul(100)));
+        // const apr = Number(pairData.apr) * 100 + rewardsApr;
+        const compounding = calcCompoundingApy(rewardsApr);
+
+        const apy = Number(pairData.apr) * 100 + compounding + rewardsApr; // RewardsApr is included in compounding
+        return {
+            feeApr: Number(pairData.apr) * 100,
+            rewardsApr,
+            apy,
+            compounding,
+        };
+    } catch (error) {
+        return {
+            feeApr: 0,
+            rewardsApr: 0,
+            apy: 0,
+            compounding: 0,
+        };
     }
-    // const cakePerYearUsd = cakePerYear.mul(cakePrice).mul(allocPoint).div(totalAllocPoint);
-    // let rewardsApr = Number(toEth(cakePerYearUsd.div(pairData.liquidityUSD).mul(100)));
-    // const apr = Number(pairData.apr) * 100 + rewardsApr;
-    const compounding = calcCompoundingApy(rewardsApr);
-
-    const apy = Number(pairData.apr) * 100 + compounding + rewardsApr; // RewardsApr is included in compounding
-    return {
-        feeApr: Number(pairData.apr) * 100,
-        rewardsApr,
-        apy,
-        compounding,
-    };
 };
 
 const getDodoApy = async (pairAddress: string, provider: providers.Provider, chainId: number) => {
-    const res = await axios.post(DODO_GRAPH_URL, {
-        query: `
+    try {
+        const res = await axios.post(DODO_GRAPH_URL, {
+            query: `
     {
         pools(where: {lpToken:"${pairAddress.toLowerCase()}"}) {
             id
@@ -189,59 +209,67 @@ const getDodoApy = async (pairAddress: string, provider: providers.Provider, cha
         }
       }
     `,
-    });
-    const pairData = res.data.data.lpToken.pair as {
-        feeUSD: string;
-        volumeUSD: string;
-        quoteReserve: string;
-        baseReserve: string;
-    };
-    const data = {
-        ...pairData,
-        staked: Number(res.data.data.pools[0].staked),
-    };
+        });
+        const pairData = res.data.data.lpToken.pair as {
+            feeUSD: string;
+            volumeUSD: string;
+            quoteReserve: string;
+            baseReserve: string;
+        };
+        const data = {
+            ...pairData,
+            staked: Number(res.data.data.pools[0].staked),
+        };
 
-    const price = await getPrice(addressesByChainId[chainId].dodoTokenAddress, chainId);
-    const mineContract = new Contract(addressesByChainId[chainId].dodoMineAddress, dodoMineAbi, provider);
-    const latestBlock = await provider.getBlockNumber();
-    const blocksAmount = 200000;
+        const price = await getPrice(addressesByChainId[chainId].dodoTokenAddress, chainId);
+        const mineContract = new Contract(addressesByChainId[chainId].dodoMineAddress, dodoMineAbi, provider);
+        const latestBlock = await provider.getBlockNumber();
+        const blocksAmount = 200000;
 
-    const [
-        { timestamp: latestBlockTimestamp },
-        { timestamp: oldBlockTimestamp },
-        dodoPerBlock,
-        { allocPoint },
-        totalAlloc,
-    ] = await Promise.all([
-        provider.getBlock(latestBlock),
-        provider.getBlock(latestBlock - blocksAmount),
-        mineContract.dodoPerBlock(),
-        mineContract.poolInfos(4),
-        mineContract.totalAllocPoint(),
-    ]);
-    const rewardPerBlock = Number(toEth(dodoPerBlock));
-    const difference = latestBlockTimestamp - oldBlockTimestamp;
-    const blocksPerDay = (difference / blocksAmount) * 86400;
-    const numOfBlocksPerDay = blocksPerDay;
-    const rewardPerDay = rewardPerBlock * numOfBlocksPerDay;
-    const rewardPerYear = rewardPerDay * 365;
-    let alloc = allocPoint.mul(100000).div(totalAlloc);
+        const [
+            { timestamp: latestBlockTimestamp },
+            { timestamp: oldBlockTimestamp },
+            dodoPerBlock,
+            { allocPoint },
+            totalAlloc,
+        ] = await Promise.all([
+            provider.getBlock(latestBlock),
+            provider.getBlock(latestBlock - blocksAmount),
+            mineContract.dodoPerBlock(),
+            mineContract.poolInfos(4),
+            mineContract.totalAllocPoint(),
+        ]);
+        const rewardPerBlock = Number(toEth(dodoPerBlock));
+        const difference = latestBlockTimestamp - oldBlockTimestamp;
+        const blocksPerDay = (difference / blocksAmount) * 86400;
+        const numOfBlocksPerDay = blocksPerDay;
+        const rewardPerDay = rewardPerBlock * numOfBlocksPerDay;
+        const rewardPerYear = rewardPerDay * 365;
+        let alloc = allocPoint.mul(100000).div(totalAlloc);
 
-    const rewardPerYearUsd = rewardPerYear * price * (alloc.toNumber() / 100000);
-    const constant = 4.1;
-    // const constant = 1;
+        const rewardPerYearUsd = rewardPerYear * price * (alloc.toNumber() / 100000);
+        const constant = 4.1;
+        // const constant = 1;
 
-    let apr = (rewardPerYearUsd / data.staked) * 100;
-    apr /= constant;
+        let apr = (rewardPerYearUsd / data.staked) * 100;
+        apr /= constant;
 
-    const compounding = calcCompoundingApy(apr);
-    const apy = compounding + apr;
-    return {
-        feeApr: 0,
-        rewardsApr: apr,
-        apy,
-        compounding,
-    };
+        const compounding = calcCompoundingApy(apr);
+        const apy = compounding + apr;
+        return {
+            feeApr: 0,
+            rewardsApr: apr,
+            apy,
+            compounding,
+        };
+    } catch (error) {
+        return {
+            feeApr: 0,
+            rewardsApr: 0,
+            apy: 0,
+            compounding: 0,
+        };
+    }
 };
 
 const getFraxApy = async () => {
@@ -308,41 +336,31 @@ export const getApy = async (
     if (chainId !== defaultChainId) {
         return {
             feeApr: 0,
-            rewardsApr: Number(farm.rewards_apy || 0),
-            apy: Number(farm.total_apy || 0),
+            rewardsApr: 0,
+            apy: 0,
             compounding: 0,
         };
     }
-    try {
-        switch (farm.originPlatform) {
-            case FarmOriginPlatform.Shushiswap:
-                return getSushiswapApy(farm.lp_address.toLowerCase(), chainId, provider);
-            case FarmOriginPlatform.GMX:
-                return getGmxApyArbitrum(provider, currentWallet);
-            case FarmOriginPlatform.Dodo:
-                return getDodoApy(farm.lp_address, provider, chainId);
-            case FarmOriginPlatform.Frax:
-                return getFraxApy();
-            case FarmOriginPlatform.SwapFish:
-                return getSwapFishApy(farm.lp_address, chainId, provider, farm.pool_id!);
-            case FarmOriginPlatform.Hop:
-                return getHopApy(farm.name, chainId);
+    switch (farm.originPlatform) {
+        case FarmOriginPlatform.Shushiswap:
+            return getSushiswapApy(farm.lp_address.toLowerCase(), chainId, provider);
+        case FarmOriginPlatform.GMX:
+            return getGmxApyArbitrum(provider, currentWallet);
+        case FarmOriginPlatform.Dodo:
+            return getDodoApy(farm.lp_address, provider, chainId);
+        case FarmOriginPlatform.Frax:
+            return getFraxApy();
+        case FarmOriginPlatform.SwapFish:
+            return getSwapFishApy(farm.lp_address, chainId, provider, farm.pool_id!);
+        case FarmOriginPlatform.Hop:
+            return getHopApy(farm.name, chainId);
 
-            default:
-                return {
-                    feeApr: 0,
-                    rewardsApr: Number(farm.rewards_apy || 0),
-                    apy: Number(farm.total_apy || 0),
-                    compounding: 0,
-                };
-        }
-    } catch (err: any) {
-        console.error("Error in getting apy => ", err);
-        return {
-            feeApr: 0,
-            rewardsApr: Number(farm.rewards_apy || 0),
-            apy: Number(farm.total_apy || 0),
-            compounding: 0,
-        };
+        default:
+            return {
+                feeApr: 0,
+                rewardsApr: Number(farm.rewards_apy || 0),
+                apy: Number(farm.total_apy || 0),
+                compounding: 0,
+            };
     }
 };
