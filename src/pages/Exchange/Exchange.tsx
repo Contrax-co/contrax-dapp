ebc64f300953dff76f1e6de21bce6f3b808b7b1f
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-04-10 17:01:25 +0500

    feat: Web3auth

--- src/pages/Exchange/Exchange.tsx
+++ src/pages/Exchange/Exchange.tsx
@@ -2,33 +2,21 @@ import React, { useRef } from "react";
 import useWallet from "src/hooks/useWallet";
 import useApp from "src/hooks/useApp";
 import { Bridge } from "@socket.tech/plugin";
-import {
-    defaultChainId,
-    RAMP_TRANSAK_API_KEY,
-    SOCKET_API_KEY,
-    ZERODEV_PROJECT_ID,
-    ZERODEV_PROJECT_ID_MAINNET,
-} from "src/config/constants";
+import { defaultChainId, RAMP_TRANSAK_API_KEY, SOCKET_API_KEY } from "src/config/constants";
+// import { RampInstantSDK } from "@ramp-network/ramp-instant-sdk";
 
 import PoolButton from "src/components/PoolButton/PoolButton";
-import { SwapWidget, darkTheme, lightTheme } from "@uniswap/widgets";
+import { SwapWidget, darkTheme, lightTheme, TokenInfo } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
 import styles from "./Exchange.module.scss";
 import "./Exchange.css";
-import { useConnect, useDisconnect, useSigner, useSwitchNetwork, useWebSocketProvider } from "wagmi";
+import { useSigner, useWebSocketProvider } from "wagmi";
+import { getWeb3AuthProvider } from "src/config/walletConfig";
+import useFarms from "src/hooks/farms/useFarms";
 import { useSearchParams } from "react-router-dom";
 import useBalances from "src/hooks/useBalances";
 import { Tabs } from "src/components/Tabs/Tabs";
 import uniswapTokens from "./uniswapTokens.json";
-import {
-    googleWallet,
-    facebookWallet,
-    githubWallet,
-    discordWallet,
-    twitchWallet,
-    twitterWallet,
-} from "@zerodevapp/wagmi/rainbowkit";
-import { useAppSelector } from "src/state";
 
 interface IProps {}
 
@@ -65,11 +53,7 @@ enum Tab {
 }
 
 const Exchange: React.FC<IProps> = () => {
-    const { currentWallet, connectWallet, chains, signer: wagmiSigner, networkId, provider } = useWallet();
-    const { switchNetworkAsync } = useSwitchNetwork();
-    const { connectAsync } = useConnect();
-    const { disconnectAsync } = useDisconnect();
-    const connectorId = useAppSelector((state) => state.settings.connectorId);
+    const { currentWallet, connectWallet, chains, signer: wagmiSigner } = useWallet();
     const [chainId, setChainId] = React.useState<number>(defaultChainId);
     const { data: signer } = useSigner({
         chainId,
@@ -79,8 +63,11 @@ const Exchange: React.FC<IProps> = () => {
     const { reloadBalances } = useBalances();
 
     const { lightMode } = useApp();
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [provider, setProvider] = React.useState<any>();
     const websocketProvider = useWebSocketProvider();
     const [tab, setTab] = React.useState<Tab>(Tab.Buy);
+    const [isWeb3Auth, setIsWeb3Auth] = React.useState(false);
 
     // Reload Balances every time this component unmounts
     React.useEffect(() => reloadBalances, []);
@@ -96,63 +83,66 @@ const Exchange: React.FC<IProps> = () => {
             });
     }, [params]);
 
+    // React.useEffect(() => {
+    //     if (tab === Tab.Buy) {
+    //         const ramp = new RampInstantSDK({
+    //             userAddress: currentWallet,
+    //             defaultAsset: "ARBITRUM_USDC",
+    //             swapAsset: "ARBITRUM_*",
+    //             fiatValue: "500",
+    //             fiatCurrency: "USD",
+    //             hostAppName: "Contrax",
+    //             hostLogoUrl: `https://${window.location.host}/logo.svg`,
+    //             hostApiKey: RAMP_SDK_HOST_API_KEY,
+    //             variant: "embedded-mobile",
+    //             containerNode: containerRef.current || undefined,
+    //         })
+    //             // @ts-ignore
+    //             .on("PURCHASE_CREATED", reloadBalances)
+    //             .show();
+    //         return () => {
+    //             ramp.close();
+    //         };
+    //     }
+    // }, [containerRef, tab, currentWallet]);
+
     const handleBridgeNetworkChange = async () => {
         try {
-            console.log("network changted");
-            if (networkId === chainId || !connectorId) return;
-            try {
-                await disconnectAsync();
-            } catch {}
-            let connector: any;
-            let projectId = chainId === defaultChainId ? ZERODEV_PROJECT_ID : ZERODEV_PROJECT_ID_MAINNET;
-            switch (connectorId) {
-                case "github":
-                    connector = githubWallet({ options: { projectId } });
-                    break;
-                case "google":
-                    connector = googleWallet({ options: { projectId } });
-                    break;
-                case "facebook":
-                    connector = facebookWallet({ options: { projectId } });
-                    break;
-                case "discord":
-                    connector = discordWallet({ options: { projectId } });
-                    break;
-                case "twitch":
-                    connector = twitchWallet({ options: { projectId } });
-                    break;
-                case "twitter":
-                    connector = twitterWallet({ options: { projectId } });
-                    break;
-                default:
-                    switchNetworkAsync && (await switchNetworkAsync(chainId));
-                    return;
+            // @ts-ignore
+            const pkey = await signer?.provider?.provider?.request({ method: "eth_private_key" });
+
+            if (!pkey) {
+                setIsWeb3Auth(true);
+                setProvider(undefined);
+                return;
             }
-            console.log(connector);
-            let created = connector.createConnector();
-            console.log(created);
-            await connectAsync(created);
-        } catch (err: any) {
-            console.error(err);
+            const chain = chains.find((c) => c.id === chainId);
+            const _provider = await getWeb3AuthProvider({
+                chainId: chain?.id!,
+                blockExplorer: chain?.blockExplorers?.default.url!,
+                name: chain?.name!,
+                rpc: chain?.rpcUrls.public.http[0]!,
+                ticker: chain?.nativeCurrency.symbol!,
+                tickerName: chain?.nativeCurrency.name!,
+                pkey,
+            });
+            setProvider(_provider);
+            setIsWeb3Auth(true);
+        } catch {
+            // switchNetworkAsync && (await switchNetworkAsync(chainId));
+            setIsWeb3Auth(false);
         }
     };
 
     React.useEffect(() => {
-        const timeout = setTimeout(() => {
-            if (tab === Tab.Bridge) handleBridgeNetworkChange();
-        }, 3000);
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [currentWallet, chainId, signer, tab, connectorId, networkId]);
+        if (tab === Tab.Bridge) handleBridgeNetworkChange();
+    }, [currentWallet, chainId, signer, tab]);
 
     React.useEffect(() => {
         if (tab !== Tab.Bridge) {
             setChainId(defaultChainId);
         }
     }, [tab]);
-
-    console.log(wagmiSigner, provider);
 
     return (
         <div
@@ -218,7 +208,7 @@ const Exchange: React.FC<IProps> = () => {
                 )}
                 {tab === Tab.Bridge && SOCKET_API_KEY && (
                     <Bridge
-                        provider={wagmiSigner?.provider}
+                        provider={isWeb3Auth ? provider : signer?.provider}
                         onSourceNetworkChange={(network) => {
                             setChainId(network.chainId);
                         }}
@@ -241,8 +231,8 @@ const Exchange: React.FC<IProps> = () => {
                             "optimism-bridge",
                             "refuel-bridge",
                         ]}
-                        defaultSourceNetwork={defaultChainId}
-                        defaultDestNetwork={1}
+                        defaultSourceNetwork={1}
+                        defaultDestNetwork={defaultChainId}
                         sourceNetworks={[1, defaultChainId]}
                         destNetworks={[1, defaultChainId]}
                         customize={lightMode ? lightSocketTheme : darkSocketTheme}
@@ -263,7 +253,7 @@ const Exchange: React.FC<IProps> = () => {
                                 : { ...darkTheme, accent: "#63cce0", accentSoft: "#dcf9ff" }
                         }
                         // @ts-ignore
-                        provider={wagmiSigner.zdProvider}
+                        provider={websocketProvider || wagmiSigner?.provider}
                         onConnectWalletClick={connectWallet}
                         onTxSuccess={reloadBalances}
                         tokenList={uniswapTokens}
