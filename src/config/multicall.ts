a3eb2425a9768720ad28823e35f84043a3e0e5eb
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-04-12 12:11:07 +0500

    chore: multicall batch size -> 150

--- src/config/multicall.ts
+++ src/config/multicall.ts
@@ -4,7 +4,7 @@ import ethers from "ethers";
 const multicall = new Multicall();
 Multicall.DefaultOptions.verbose = true;
 Multicall.DefaultOptions.timeWindow = 500;
-Multicall.DefaultOptions.batchSize = 100;
+Multicall.DefaultOptions.batchSize = 150;
 
 export const getMulticallProvider = (provider: ethers.providers.Provider) => {
     return new providers.MulticallProvider(provider, multicall);
