ab2c2d5e1b9819e6d2daff166e12740a2a0a6193
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-04-12 14:37:15 +0500

    fix: on changing farm, set currency to USDC

--- src/components/FarmItem/FarmRow.tsx
+++ src/components/FarmItem/FarmRow.tsx
@@ -34,7 +34,7 @@ const FarmRow: React.FC<Props> = ({ farm, openedFarm, setOpenedFarm }) => {
 
     const handleClick = () => {
         setDropDown((prev) => !prev);
-        dispatch(setFarmDetailInputOptions({ transactionType: FarmTransactionType.Deposit }));
+        dispatch(setFarmDetailInputOptions({ transactionType: FarmTransactionType.Deposit, currencySymbol: "USDC" }));
         if (farm) setOpenedFarm(openedFarm === farm.id ? undefined : farm.id);
     };
 
