ed3f31d38430c67970340b5ac14a097a179c2652
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-04-12 11:56:16 +0500

    chore: remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -46,17 +46,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("./service-worker.js")
-            .then((registration) => {
-                console.log("Service worker registered:", registration);
-            })
-            .catch((error) => {
-                console.log("Service worker registration failed:", error);
-            });
-    });
-}
-
 reportWebVitals();
