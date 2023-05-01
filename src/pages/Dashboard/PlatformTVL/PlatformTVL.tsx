bb38b376c059ef6f3bb5ac1dfc7bbbd9474679ff
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-05-01 17:16:48 +0500

    feat: added platform tvl

---
+++ src/pages/Dashboard/PlatformTVL/PlatformTVL.tsx
@@ -0,0 +1,22 @@
+import useApp from "src/hooks/useApp";
+import "./PlatformTVL.css";
+import useTVL from "src/hooks/useTVL";
+import tvlIcon from "src/assets/images/tvl-icon.png";
+
+interface Props {}
+
+const VaultItem: React.FC<Props> = () => {
+    const { lightMode } = useApp();
+    const tvl = useTVL();
+
+    if (tvl === 0) return <div></div>;
+
+    return (
+        <div className={`tvl-container`}>
+            {<img className={`tvl-image`} alt={"total value locked"} src={tvlIcon} />}
+            <p className={`tvl-value`}>${tvl.toFixed(0)}</p> <p className={`tvl-title`}>TVL</p>
+        </div>
+    );
+};
+
+export default VaultItem;
