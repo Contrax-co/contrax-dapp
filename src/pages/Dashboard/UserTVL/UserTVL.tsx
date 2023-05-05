77a8d9c08d8877470546caf571c5b06d8f755d37
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-05-05 16:55:41 +0500

    style: userTVL component re-designed

--- src/pages/Dashboard/UserTVL/UserTVL.tsx
+++ src/pages/Dashboard/UserTVL/UserTVL.tsx
@@ -1,7 +1,6 @@
-import "./UserTVL.css";
 import useTVL from "src/hooks/useTVL";
-import tvlIcon from "src/assets/images/tvl-icon.png";
 import { commify } from "ethers/lib/utils.js";
+import styles from "./UserTVL.module.scss";
 
 interface Props {}
 
@@ -11,9 +10,9 @@ const VaultItem: React.FC<Props> = () => {
     if (userTVL === 0) return <div></div>;
 
     return (
-        <div className={`tvl-container`}>
-            {<img className={`tvl-image`} alt={"total value locked"} src={tvlIcon} />}
-            <p className={`tvl-value`}>${commify(userTVL.toFixed(0))}</p> <p className={`tvl-title`}>My TVL</p>
+        <div className={styles.tvlContainer}>
+            <p className={styles.tvlHeading}>My Total Value Locked</p>
+            <p className={styles.tvlValue}>${commify(userTVL.toFixed(0))}</p>
         </div>
     );
 };
