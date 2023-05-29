9233fbeb05b6b7bf875d7ccbe4d3921b5d94553c
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-05-29 18:19:58 +0500

    chore: front secrets

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -34,3 +34,7 @@ export const TENDERLY_PROJECT_SLUG = process.env.REACT_APP_TENDERLY_PROJECT_SLUG
 export const TENDERLY_USER_NAME = process.env.REACT_APP_TENDERLY_USER_NAME;
 export const WERT_PARTNER_ID = process.env.REACT_APP_WERT_PARTNER_ID;
 export const INFURA_KEY = process.env.REACT_APP_INFURA;
+
+export const FRONT_URL = process.env.REACT_APP_FRONT_URL as string;
+export const FRONT_API_KEY = process.env.REACT_APP_FRONT_API_KEY as string;
+export const FRONT_CLIENT_ID = process.env.REACT_APP_FRONT_CLIENT_ID as string;
