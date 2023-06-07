d272cd91bb4a138e4d7ba448f7a8b7faa2fbacd8
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-06-07 18:03:22 +0500

    feat: stats table ui made dynamic

--- src/components/StatsTable/StatsTable.tsx
+++ src/components/StatsTable/StatsTable.tsx
@@ -33,11 +33,14 @@ export const StatsTable: FC = () => {
     };
 
     return (
-        <table className={styles.table}>
-            <thead className={styles.tableHeader}>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData} onClick={() => handleSorting(TableColumns.Address)}>
-                        <p className={styles.columnHeading}>
+        <div className={styles.container}>
+            <table className={styles.table}>
+                <tr className={styles.header}>
+                    <th>
+                        <div
+                            className={styles.tableData + " " + styles.heading}
+                            onClick={() => handleSorting(TableColumns.Address)}
+                        >
                             ADDRESS
                             {sortBy === TableColumns.Address && (
                                 <FaArrowDown
@@ -45,62 +48,108 @@ export const StatsTable: FC = () => {
                                     className={`${styles.transitionDelay} ${order === "" ? "" : styles.rotate}`}
                                 />
                             )}
-                        </p>
-                    </td>
-                    <td className={styles.tableData} onClick={() => handleSorting(TableColumns.TVL)}>
-                        <p className={styles.columnHeading}>
-                            VALUE
+                        </div>
+                    </th>
+                    <th>
+                        <div
+                            className={styles.tableData + " " + styles.heading}
+                            onClick={() => handleSorting(TableColumns.Referrer)}
+                        >
+                            REFERRER
+                            {sortBy === TableColumns.Referrer && (
+                                <FaArrowDown
+                                    size={14}
+                                    className={`${styles.transitionDelay} ${order === "" ? "" : styles.rotate}`}
+                                />
+                            )}
+                        </div>
+                    </th>
+                    <th>
+                        <div
+                            className={styles.tableData + " " + styles.heading}
+                            onClick={() => handleSorting(TableColumns.TVL)}
+                        >
+                            TVL
                             {sortBy === TableColumns.TVL && (
                                 <FaArrowDown
                                     size={14}
                                     className={`${styles.transitionDelay} ${order === "" ? "" : styles.rotate}`}
                                 />
                             )}
-                        </p>
-                    </td>
+                        </div>
+                    </th>
                 </tr>
-            </thead>
-            <tbody className={styles.tableBody}>
                 {userTVLs && userTVLs.length > 0 ? (
                     userTVLs.map(({ id, address, tvl }) => (
                         <tr key={id} className={styles.tableRow}>
-                            <td className={styles.tableData + " " + styles.addressCol}>
-                                <p className={styles.addressText + " " + styles.onlyExtraLargeScreen}>{address}</p>
-                                <p className={styles.addressText + " " + styles.onlyLargeScreen}>
-                                    {`${address?.substring(0, 12)}...${address?.substring(address.length - 12)}`}{" "}
-                                    <div className={styles.tooltip}>{address}</div>
-                                </p>
-                                <p className={styles.addressText + " " + styles.onlyTablet}>
-                                    {`${address?.substring(0, 8)}...${address?.substring(address.length - 8)}`}{" "}
-                                    <div className={styles.tooltip}>{address}</div>
-                                </p>
-                                <p className={styles.addressText + " " + styles.onlyMobile}>
-                                    {`${address?.substring(0, 6)}...${address?.substring(address.length - 4)}`}{" "}
-                                    <div className={styles.tooltip}>{address}</div>
-                                </p>
-                                <FiExternalLink
-                                    size={16}
-                                    className={styles.arbiscanIcon}
-                                    onClick={() => window.open(`${BLOCK_EXPLORER_URL}/address/${address}`, "_blank")}
-                                />
+                            <td>
+                                <div className={styles.tableData + " " + styles.addressCol}>
+                                    <p className={styles.addressText + " " + styles.onlyExtraLargeScreen}>{address}</p>
+                                    <p className={styles.addressText + " " + styles.onlyLargeScreen}>
+                                        {`${address?.substring(0, 12)}...${address?.substring(address.length - 12)}`}{" "}
+                                        <div className={styles.tooltip}>{address}</div>
+                                    </p>
+                                    <p className={styles.addressText + " " + styles.onlyTablet}>
+                                        {`${address?.substring(0, 8)}...${address?.substring(address.length - 8)}`}{" "}
+                                        <div className={styles.tooltip}>{address}</div>
+                                    </p>
+                                    <p className={styles.addressText + " " + styles.onlyMobile}>
+                                        {`${address?.substring(0, 5)}...${address?.substring(address.length - 3)}`}{" "}
+                                        <div className={styles.tooltip}>{address}</div>
+                                    </p>
+                                    <FiExternalLink
+                                        size={16}
+                                        className={styles.arbiscanIcon}
+                                        onClick={() =>
+                                            window.open(`${BLOCK_EXPLORER_URL}/address/${address}`, "_blank")
+                                        }
+                                    />
+                                </div>
                             </td>
-                            <td className={styles.tableData}>
-                                {customCommify(tvl, { minimumFractionDigits: 1, showDollarSign: true }).slice(0, -2)}
+                            <td>
+                                <div className={styles.tableData + " " + styles.addressCol}>
+                                    <p className={styles.addressText + " " + styles.onlyExtraLargeScreen}>{address}</p>
+                                    <p className={styles.addressText + " " + styles.onlyLargeScreen}>
+                                        {`${address?.substring(0, 12)}...${address?.substring(address.length - 12)}`}{" "}
+                                        <div className={styles.tooltip}>{address}</div>
+                                    </p>
+                                    <p className={styles.addressText + " " + styles.onlyTablet}>
+                                        {`${address?.substring(0, 8)}...${address?.substring(address.length - 8)}`}{" "}
+                                        <div className={styles.tooltip}>{address}</div>
+                                    </p>
+                                    <p className={styles.addressText + " " + styles.onlyMobile}>
+                                        {`${address?.substring(0, 5)}...${address?.substring(address.length - 3)}`}{" "}
+                                        <div className={styles.tooltip}>{address}</div>
+                                    </p>
+                                    <FiExternalLink
+                                        size={16}
+                                        className={styles.arbiscanIcon}
+                                        onClick={() =>
+                                            window.open(`${BLOCK_EXPLORER_URL}/address/${address}`, "_blank")
+                                        }
+                                    />
+                                </div>
+                            </td>
+                            <td>
+                                <div className={styles.tableData}>
+                                    {customCommify(tvl, { minimumFractionDigits: 1, showDollarSign: true }).slice(
+                                        0,
+                                        -2
+                                    )}
+                                </div>
                             </td>
                         </tr>
                     ))
                 ) : (
                     <tr className={styles.tableRow}>
-                        <td colSpan={2}>
+                        <td colSpan={3}>
                             <EmptyTable />
                         </td>
                     </tr>
                 )}
-            </tbody>
-            <tfoot className={styles.tableFooter}>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData}>
-                        <div className={styles.searchBox}>
+                <tr className={styles.footer}>
+                    <td>
+                        <div className={styles.tableData + " " + styles.searchBox}>
                             <FaSearch className={styles.onlyMobile} />
                             <label htmlFor="searchBox" className={styles.onlyDesktop}>
                                 Search:
@@ -117,26 +166,28 @@ export const StatsTable: FC = () => {
                             />
                         </div>
                     </td>
-                    <td className={styles.tableData + " " + styles.controls}>
-                        {hasPrevPage && (
-                            <FaArrowLeft
-                                className={styles.pageChangeArrow}
-                                onClick={() => setPage((prev) => prev - 1)}
-                            />
-                        )}
-                        <p>
-                            Page {page} of {totalPages}
-                        </p>
-                        {hasNextPage && (
-                            <FaArrowRight
-                                className={styles.pageChangeArrow}
-                                onClick={() => setPage((prev) => prev + 1)}
-                            />
-                        )}
+                    <td colSpan={2}>
+                        <div className={styles.tableData + " " + styles.controls}>
+                            {hasPrevPage && (
+                                <FaArrowLeft
+                                    className={styles.pageChangeArrow}
+                                    onClick={() => setPage((prev) => prev - 1)}
+                                />
+                            )}
+                            <p>
+                                Page {page} of {totalPages}
+                            </p>
+                            {hasNextPage && (
+                                <FaArrowRight
+                                    className={styles.pageChangeArrow}
+                                    onClick={() => setPage((prev) => prev + 1)}
+                                />
+                            )}
+                        </div>
                     </td>
                 </tr>
-            </tfoot>
-        </table>
+            </table>
+        </div>
     );
 };
 
