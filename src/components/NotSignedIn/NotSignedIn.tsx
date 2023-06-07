b3d89f77f91bb5c06f6f8bfbd9598d44fe7f4173
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-06-07 12:20:35 +0500

    feat: NotSignedIn component upgraded for dynamic content

--- src/components/NotSignedIn/NotSignedIn.tsx
+++ src/components/NotSignedIn/NotSignedIn.tsx
@@ -1,14 +1,25 @@
 import { ReactComponent as WalletSvg } from "src/assets/images/walletSvg.svg";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "./NotSignedIn.module.scss";
+import { FC } from "react";
 
-export const NotSignedIn = () => {
+interface Props {
+    heading?: string;
+    description?: string;
+    buttonText?: string;
+}
+
+export const NotSignedIn: FC<Props> = ({
+    heading = "Please Sign In",
+    description = "Sign in or sign up to use this page",
+    buttonText = "Sign In/Up",
+}) => {
     return (
         <div className={styles.sign_in_placeholder}>
             <WalletSvg className={styles.walletIcon} />
-            <p className={styles.disclaimer}>Please Sign In</p>
-            <p className={styles.description}>Sign in or sign up to use this page</p>
-            <ConnectButton label="Sign In/Up" />
+            <p className={styles.disclaimer}>{heading}</p>
+            <p className={styles.description}>{description}</p>
+            <ConnectButton label={buttonText} />
         </div>
     );
 };
