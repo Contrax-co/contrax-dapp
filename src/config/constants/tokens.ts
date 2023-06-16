d514a7156c8768299268a0007ba2c829ce114f52
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-06-16 12:58:33 +0500

    chore: native usdc name change

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -3,7 +3,7 @@ import { CHAIN_ID } from "src/types/enums";
 const tokens = [
     {
         address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-        name: "USDC Native",
+        name: "Native USDC",
         logo: "https://raw.githubusercontent.com/Contrax-co/tokens/main/arbitrum-tokens/0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8/logo.png",
         decimals: 6,
         chainId: CHAIN_ID.ARBITRUM,
