a5363bd7e186f69d4d3837557e15c57e5fa4c48b
abdulrafay-contrax <abdulrafay@contrax.finance>
2023-06-27 18:44:56 +0500

    chore: update front icon on sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,6 +8,7 @@ import { ImStatsDots } from "react-icons/im";
 import { IoIosFlask } from "react-icons/io";
 import { ReactComponent as EarnIcon } from "src/assets/images/earn.svg";
 import { ReactComponent as DepositIcon } from "src/assets/images/deposit.svg";
+import { ReactComponent as FrontIcon } from "src/assets/images/front.svg";
 import logo from "src/assets/images/logo.png";
 import logo2 from "src/assets/images/logo-4x.png";
 import LightModeToggle from "src/components/LightModeToggle/LightModeToggle";
@@ -68,7 +69,7 @@ function Sidebar({ handleClose }: { handleClose: Dispatch<SetStateAction<boolean
                 />
                 <SidebarItem
                     title="Front"
-                    icon={<DepositIcon width={16} />}
+                    icon={<FrontIcon width={18} />}
                     onClick={() => handleNavigation(RoutesPaths.Deposit)}
                     active={pathname === RoutesPaths.Deposit}
                 />
