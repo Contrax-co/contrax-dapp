093a7833c1680d7d1bda8f4c610d35f77c10ae70
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-02-16 18:20:16 +0500

    fix : create two button for USDC transfer

--- src/components/SwapUSDCBtn/SwapUSDCBtn.tsx
+++ src/components/SwapUSDCBtn/SwapUSDCBtn.tsx
@@ -3,7 +3,7 @@ import styles from "./SwapUSDCBtn.module.scss";
 import { TiWarningOutline } from "react-icons/ti";
 import { customCommify } from "src/utils/common";
 import useSwapUsdcNative from "src/hooks/useSwapUsdcNative";
-import { ReactComponent as EditSvg } from "src/assets/images/edit.svg";
+// import { ReactComponent as EditSvg } from "src/assets/images/edit.svg";
 import NativeUSDC from "../modals/NativeUSDC/NativeUSDC";
 
 interface IProps {
@@ -16,9 +16,9 @@ const SwapUSDCBtn: React.FC<IProps> = ({ showDisclaimer }) => {
 
     return formattedBalance > 0.1 ? (
         <div className={`outlinedContainer ${styles.container}`}>
-            <div className={styles.editIconContainer}>
+            {/* <div className={styles.editIconContainer}>
                 <EditSvg className={styles.editIcon} onClick={() => setNativeModal(true)} />
-            </div>
+            </div> */}
             <div className={styles.labeledButton}>
                 <div>
                     <h3 className={styles.usdcAmount}>
@@ -26,14 +26,19 @@ const SwapUSDCBtn: React.FC<IProps> = ({ showDisclaimer }) => {
                     </h3>
                 </div>
             </div>
-            <button
-                className={`custom-button ${styles.bridgeButton}`}
-                type="submit"
-                disabled={loading}
-                onClick={() => initateSwap()}
-            >
-                {loading ? "Swapping..." : "Swap to USDC"}
-            </button>
+            <div className={styles.USDCBtnContainer}>
+                <button
+                    className={`custom-button ${styles.bridgeButton}`}
+                    type="submit"
+                    disabled={loading}
+                    onClick={() => initateSwap()}
+                >
+                    {loading ? "Swapping..." : "Swap All"}
+                </button>
+                <button className={`custom-button ${styles.bridgeButton}`} onClick={() => setNativeModal(true)}>
+                    Custom Amount
+                </button>
+            </div>
             <p className={styles.disclaimer}>
                 <TiWarningOutline size={12} className={styles.disclaimerLogo} />
                 This will swap your entire Native USDC balance over to USDC.
