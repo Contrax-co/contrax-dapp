cd4d3e5b5cf14584b7c699735cc5844e83f2c12f
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-02-21 19:17:53 +0500

    chore : add random description and add a referral link for sign user on referral dashboard

--- src/pages/ReferralDashboard/ReferralDashboard.tsx
+++ src/pages/ReferralDashboard/ReferralDashboard.tsx
@@ -4,15 +4,32 @@ import { ReferralCard } from "src/components/ReferralCard/ReferralCard";
 import { ReferralDashboardTable } from "src/components/ReferralDashboardTable/ReferralDashboardTable";
 import { useReferralDashboard } from "src/hooks/useReferralDashboard";
 import { getPositionSuffix } from "src/utils";
+import useAccountData from "src/hooks/useAccountData";
+import useWallet from "src/hooks/useWallet";
 
 const ReferralDashboard: React.FC = () => {
+    const { referralLink } = useAccountData();
+    const { currentWallet } = useWallet();
     const { data } = useReferralDashboard();
     const topThreeReferrals = useMemo(() => {
         return data?.sort((a, b) => b.tvlFromReferrals - a.tvlFromReferrals).slice(0, 3) ?? [];
     }, [data]);
     return (
         <div className={styles.container}>
-            <p className={styles.para}>Lorem ipsum dolor sit amet consectetur </p>
+            <h1 className={styles.mainHeading}>Intract Referral Contest</h1>
+            <p className={styles.para}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum numquam optio magnam laborum,
+                sapiente ipsum maiores, quo iusto atque fugiat, sunt quis quos velit perferendis ex sint itaque! Animi,
+                excepturi?{" "}
+            </p>
+            <p className={styles.para}>
+                <b>Join the race with your referral link:</b>{" "}
+                {currentWallet && referralLink ? (
+                    <span className={styles.text}>{referralLink}</span>
+                ) : (
+                    <span className={styles.text}>Please sign in to see it here</span>
+                )}
+            </p>
             <div className={styles.topRow}>
                 {topThreeReferrals.length > 0 &&
                     topThreeReferrals.map((referral, i) => (
