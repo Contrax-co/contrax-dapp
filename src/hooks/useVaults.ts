b35fb0526af93f3c3ca9ce4d21cb8f2c52bc2812
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-02-27 11:44:35 +0500

    fix: staked tokens price

--- src/hooks/useVaults.ts
+++ src/hooks/useVaults.ts
@@ -1,6 +1,5 @@
 import { useMemo } from "react";
 import { Vault } from "src/types";
-import { getLpAddressForFarmsPrice } from "src/utils/common";
 import { useFarmApys } from "./farms/useFarmApy";
 import useFarms from "./farms/useFarms";
 import useBalances from "./useBalances";
@@ -23,11 +22,10 @@ export const useVaults = (): { vaults: Vault[]; isLoading: boolean; isFetched: b
     const vaults = useMemo(() => {
         return farms
             .map((farm) => {
-                const lpAddress = getLpAddressForFarmsPrice([farm])[0];
                 return {
                     ...farm,
                     userVaultBalance: usersVaultBalances[farm.vault_addr] || 0,
-                    priceOfSingleToken: priceOfSingleToken[lpAddress] || (farm.stableCoin ? 1 : 0),
+                    priceOfSingleToken: priceOfSingleToken[farm.vault_addr] || (farm.stableCoin ? 1 : 0),
                     apys: apys[farm.id],
                 };
             })
