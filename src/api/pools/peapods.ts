98fceba57f3f5d09fb5ceb94793489cfd8f0713a
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-03-27 15:54:19 +0500

    chore: deposit gmx in apGmx

--- src/api/pools/peapods.ts
+++ src/api/pools/peapods.ts
@@ -26,7 +26,7 @@ import { slippageIn, slippageOut, zapInBase, zapOutBase } from "./common";
 import { TenderlySimulateTransactionBody } from "src/types/tenderly";
 import { filterAssetChanges, filterStateDiff, getAllowanceStateOverride, simulateTransaction } from "../tenderly";
 
-let peapods = function (farmId: number): Omit<FarmFunctions, "deposit" | "withdraw"> {
+let peapods = function (farmId: number): FarmFunctions {
     const farm = pools.find((farm) => farm.id === farmId) as Farm;
 
     const getProcessedFarmData: GetFarmDataProcessedFn = (balances, prices, decimals, vaultTotalSupply) => {
@@ -104,6 +104,166 @@ let peapods = function (farmId: number): Omit<FarmFunctions, "deposit" | "withdr
         };
         return result;
     };
+    const deposit: DepositFn = async ({ amountInWei, currentWallet, signer, max }) => {
+        if (!signer) return;
+        let notiId = notifyLoading(loadingMessages.approvingDeposit());
+        try {
+            const vaultContract = new Contract(farm.vault_addr, farm.vault_abi, signer);
+
+            const lpBalance = await getBalance(farm.lp_address, currentWallet, signer.provider!);
+
+            // approve the vault to spend asset
+            await approveErc20(farm.lp_address, farm.vault_addr, lpBalance, currentWallet, signer);
+
+            dismissNotify(notiId);
+            notifyLoading(loadingMessages.confirmDeposit(), { id: notiId });
+
+            let depositTxn: any;
+            if (max) {
+                depositTxn = await vaultContract.depositAll();
+            } else {
+                depositTxn = await vaultContract.deposit(amountInWei);
+            }
+            dismissNotify(notiId);
+            notifyLoading(loadingMessages.depositing(depositTxn.transactionHash), {
+                id: notiId,
+            });
+
+            const depositTxnStatus = await depositTxn.wait(1);
+            if (!depositTxnStatus.status) {
+                throw new Error("Error depositing into vault!");
+            } else {
+                notifySuccess(successMessages.deposit());
+                dismissNotify(notiId);
+            }
+        } catch (error: any) {
+            console.log(error);
+            let err = JSON.parse(JSON.stringify(error));
+            dismissNotify(notiId);
+            notifyError(errorMessages.generalError(err.reason || err.message));
+        }
+    };
+
+    const withdraw: WithdrawFn = async ({ amountInWei, signer, max }) => {
+        if (!signer) return;
+        const notiId = notifyLoading(loadingMessages.approvingWithdraw());
+        try {
+            const vaultContract = new Contract(farm.vault_addr, farm.vault_abi, signer);
+
+            dismissNotify(notiId);
+            notifyLoading(loadingMessages.confirmingWithdraw(), { id: notiId });
+
+            let withdrawTxn: any;
+            if (max) {
+                withdrawTxn = await vaultContract.withdrawAll();
+            } else {
+                withdrawTxn = await vaultContract.withdraw(amountInWei);
+            }
+
+            dismissNotify(notiId);
+            notifyLoading(loadingMessages.withDrawing(withdrawTxn.bundleTransactionHash), {
+                id: notiId,
+            });
+
+            const withdrawTxnStatus = await withdrawTxn.wait(1);
+            if (!withdrawTxnStatus.status) {
+                throw new Error("Error withdrawing Try again!");
+            } else {
+                dismissNotify(notiId);
+                notifySuccess(successMessages.withdraw());
+            }
+        } catch (error) {
+            let err = JSON.parse(JSON.stringify(error));
+            console.log(err);
+            dismissNotify(notiId);
+            notifyError(errorMessages.generalError(err.reason || err.message));
+        }
+    };
+
+    const depositSlippage: SlippageDepositBaseFn = async ({ amountInWei, currentWallet, farm, max, signer }) => {
+        if (!signer) return BigNumber.from(0);
+        const vaultContract = new Contract(farm.vault_addr, farm.vault_abi, signer);
+
+        const transaction = {} as Omit<
+            TenderlySimulateTransactionBody,
+            "network_id" | "save" | "save_if_fails" | "simulation_type" | "state_objects"
+        >;
+        transaction.from = currentWallet;
+
+        // approve the vault to spend asset
+        transaction.state_overrides = getAllowanceStateOverride([
+            {
+                tokenAddress: farm.lp_address,
+                owner: currentWallet,
+                spender: farm.vault_addr,
+            },
+        ]);
+
+        if (max) {
+            const populated = await vaultContract.populateTransaction.depositAll();
+
+            transaction.input = populated.data || "";
+            transaction.value = populated.value?.toString();
+        } else {
+            const populated = await vaultContract.populateTransaction.deposit(amountInWei);
+
+            transaction.input = populated.data || "";
+            transaction.value = populated.value?.toString();
+        }
+        const simulationResult = await simulateTransaction({
+            /* Standard EVM Transaction object */
+            ...transaction,
+            to: farm.vault_addr,
+        });
+
+        const filteredState = filterStateDiff(farm.vault_addr, "_balances", simulationResult.stateDiffs);
+        const difference = BigNumber.from(filteredState.afterChange[currentWallet.toLowerCase()]).sub(
+            BigNumber.from(filteredState.original[currentWallet.toLowerCase()])
+        );
+
+        return difference;
+    };
+
+    const withdrawSlippage: SlippageWithdrawBaseFn = async ({
+        amountInWei,
+        currentWallet,
+        signer,
+        chainId,
+        max,
+        farm,
+    }) => {
+        if (!signer) return BigNumber.from(0);
+        const vaultContract = new Contract(farm.vault_addr, farm.vault_abi, signer);
+        const transaction = {} as Omit<
+            TenderlySimulateTransactionBody,
+            "network_id" | "save" | "save_if_fails" | "simulation_type" | "state_objects"
+        >;
+        transaction.from = currentWallet;
+
+        if (max) {
+            const populated = await vaultContract.populateTransaction.withdrawAll();
+
+            transaction.input = populated.data || "";
+            transaction.value = populated.value?.toString();
+        } else {
+            const populated = await vaultContract.populateTransaction.withdraw(amountInWei);
+
+            transaction.input = populated.data || "";
+            transaction.value = populated.value?.toString();
+        }
+
+        const simulationResult = await simulateTransaction({
+            /* Standard EVM Transaction object */
+            ...transaction,
+            to: farm.vault_addr,
+        });
+        const filteredState = filterStateDiff(farm.lp_address, "balances", simulationResult.stateDiffs);
+        const difference = BigNumber.from(filteredState.afterChange[currentWallet.toLowerCase()]).sub(
+            BigNumber.from(filteredState.original[currentWallet.toLowerCase()])
+        );
+
+        return difference;
+    };
 
     const zapIn: ZapInFn = (props) => zapInBase({ ...props, tokenIn: farm.token1, farm });
     const zapInSlippage: SlippageInBaseFn = (props) => slippageIn({ ...props, tokenIn: farm.token1, farm });
@@ -117,6 +277,10 @@ let peapods = function (farmId: number): Omit<FarmFunctions, "deposit" | "withdr
         zapOut,
         zapInSlippage,
         zapOutSlippage,
+        depositSlippage,
+        withdrawSlippage,
+        deposit,
+        withdraw,
     };
 };
 
