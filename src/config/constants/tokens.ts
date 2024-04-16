bda61eef18fe83f358464ad9ab753de13a82931e
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-04-16 13:43:30 +0500

    fix: usdc doubling in dashboard

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -3,13 +3,6 @@ import traxLogo from "./../../assets/images/contrax_leaf.png";
 
 const tokens = [
     {
-        address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-        name: "USDC",
-        logo: "https://raw.githubusercontent.com/Contrax-co/tokens/main/arbitrum-tokens/0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8/logo.png",
-        decimals: 6,
-        chainId: CHAIN_ID.ARBITRUM,
-    },
-    {
         address: "0x42Fd79DAF2a847B59D487650C68c2D7E52D752f6",
         name: "xTrax",
         logo: traxLogo,
