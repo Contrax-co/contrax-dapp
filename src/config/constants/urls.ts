80e865d1a82cf1e984a6dee61cb7526399db9a7f
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-05-23 16:00:32 +0500

    chore: links updated

--- src/config/constants/urls.ts
+++ src/config/constants/urls.ts
@@ -3,10 +3,12 @@ export const blockExplorersByChainId: { [key: number]: string } = {
 };
 
 export const paymastersByChainId: { [key: number]: string } = {
+    42161: "https://paymaster-rpc-4c40f40c9737.herokuapp.com/jrpc/42161",
     // 42161: "http://localhost:8000/jrpc/42161",
-    42161: "https://paymaster-rpc-arbitrum-cc2736d120d5.herokuapp.com/jrpc/42161",
+    // 42161: "https://paymaster-rpc-arbitrum-cc2736d120d5.herokuapp.com/jrpc/42161",
 };
 
 export const bundlersByChainId: { [key: number]: string } = {
-    42161: "https://skandha-2ct5w3uvcq-uc.a.run.app/42161",
+    42161: "https://bundler-293f2fe8c150.herokuapp.com/",
+    // 42161: "https://skandha-2ct5w3uvcq-uc.a.run.app/42161",
 };
