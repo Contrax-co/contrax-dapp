a4db802155409211e24228c501bea478dc32370c
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-06-04 17:38:46 +0500

    chore : wallet not connect handle

--- src/pages/FarmInfo/FarmInfo.tsx
+++ src/pages/FarmInfo/FarmInfo.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./FarmInfo.css";
 import useApp from "src/hooks/useApp";
 import FarmDetails from "./FarmDetails/FarmDetails";
+import useWallet from "src/hooks/useWallet";
+import { NotSignedIn } from "src/components/NotSignedIn/NotSignedIn";
 
 const FarmInfo: React.FC = () => {
     const { lightMode } = useApp();
+    const { currentWallet } = useWallet();
     return (
         <div className={`farmslip ${lightMode && "farmslip--light"}`}>
             <div className={`farmslip_header ${lightMode && "farmslip_header--light"}`}>
                 <p>Farm Info</p>
             </div>
-            <FarmDetails />
+            {currentWallet ? <FarmDetails /> : <NotSignedIn />}
         </div>
     );
 };
