4f6bc08a2674d3d9059f4265e840d5edce6896b5
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-06-27 16:37:52 +0500

    chore: login UI updated

--- src/components/modals/WalletConnectionModal/WalletConnectionModal.tsx
+++ src/components/modals/WalletConnectionModal/WalletConnectionModal.tsx
@@ -1,10 +1,10 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { ModalLayout } from "../ModalLayout/ModalLayout";
 import styles from "./WalletConnectionModal.module.scss";
 import useWallet from "src/hooks/useWallet";
 import { BeatLoader } from "react-spinners";
-import { createWalletClient, custom } from "viem";
-import { arbitrum } from "viem/chains";
+import { IoArrowForward } from "react-icons/io5";
+import { BsShieldFillCheck } from "react-icons/bs";
 
 interface IProps {
     setOpenModal: Function;
@@ -16,7 +16,15 @@ export const WalletConnectionModal: FC<IProps> = ({ setOpenModal }) => {
     const [loading, setLoading] = useState(false);
     const [showViewEmail, setShowViewEmail] = useState(false);
     const loginWithEmail = async () => {
+        const match = String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+        if (!match) return;
+        setLoading(true);
         setTimeout(() => {
+            setLoading(false);
             setShowViewEmail(true);
         }, 3000);
         await alchemySigner!.authenticate({ type: "email", email }).catch((err) => console.warn(err));
@@ -64,20 +72,53 @@ export const WalletConnectionModal: FC<IProps> = ({ setOpenModal }) => {
 
     return (
         <ModalLayout onClose={() => setOpenModal(false)} className={styles.container}>
+            <BsShieldFillCheck className={styles.warning} size={1} />
+            <h1 className={styles.heading}>Get Started</h1>
             {showViewEmail && !loading && (
                 <div className="center" style={{ height: "180px" }}>
-                    <p>View your Email for login link.</p>
+                    <p className={styles.caption}>View your Email for login link.</p>
                 </div>
             )}
             {!showViewEmail && !loading && (
                 <div>
-                    <p>Email:</p>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <button onClick={loginWithEmail}>Log In</button>
-                    <h6>OR</h6>
-                    {window.ethereum && <button onClick={connectWithInjectedWallet}>Connection with wallet</button>}
-                    <button onClick={loginWithPasskey}>Sign in with passkey</button>
-                    <button onClick={signUpWithPasskey}>Sign up with passkey</button>
+                    <p className={styles.caption}>Continue with Email</p>
+                    <div style={{ display: "flex", marginTop: 20, gap: 10 }}>
+                        <div className={styles.inputContainer}>
+                            <input
+                                type="email"
+                                placeholder="Enter your email"
+                                className={styles.input}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <button onClick={loginWithEmail} className={"custom-button center " + styles.loginBtn}>
+                            <IoArrowForward fontSize={20} />
+                        </button>
+                    </div>
+                    <p className={styles.caption} style={{ marginTop: 20 }}>
+                        OR Passkeys
+                    </p>
+                    <div className={styles.passkeyBtnsContainer}>
+                        <button onClick={loginWithPasskey} className={"custom-button " + styles.passkeyBtn}>
+                            Sign In
+                        </button>
+
+                        <button onClick={signUpWithPasskey} className={"custom-button " + styles.passkeyBtn}>
+                            Sign Up
+                        </button>
+                    </div>
+                    {window.ethereum && (
+                        <div>
+                            <div className={styles.walletDivider} />
+                            <button
+                                className={"custom-button " + styles.injectedBtn}
+                                onClick={connectWithInjectedWallet}
+                            >
+                                Continue with wallet
+                            </button>
+                        </div>
+                    )}
                 </div>
             )}
             {loading && (
